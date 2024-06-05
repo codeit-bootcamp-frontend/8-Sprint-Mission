3dcc5c4617e5815252eaec333cc935d2703c8ceb
손재헌 <jingmsi014@gmail.com>
2024-06-06 02:59:05 +0900

    Add const variables for some links

--- scripts/mainPageEventHandler.js
+++ scripts/mainPageEventHandler.js
@@ -1,7 +1,10 @@
 import { btnLink } from "./stdLib.js";
 
+const loginPage = '/login';
+const itemsPage = '/items';
+
 const loginBtn = document.querySelector('.login-button');
 const itemsBtn = document.querySelector('.items-button');
 
-loginBtn.addEventListener('click', () => { btnLink('/login') });
-itemsBtn.addEventListener('click', () => { btnLink('/items') });
+loginBtn.addEventListener('click', () => { btnLink(loginPage) });
+itemsBtn.addEventListener('click', () => { btnLink(itemsPage) });
