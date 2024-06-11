0eb74ac1a35e2c9e999ed8a34a0deb22170c953c
전주현 <scv903@naver.com>
2024-06-11 20:38:58 +0900

    Feat: add login, signup input check function
    login_signup.js 작성, 폼에 입력값 검증 기능 추가, login.html과
    signup.html에 input 태그에클레스 추기, toggle-eye 클래스 태그의 위치
    변경, login_signup.css에 자바스크립트에서 사용할 속성 추가

---
+++ js/login_signup.js
@@ -0,0 +1,166 @@
+const form = document.querySelector("form");
+const toggleEye = document.querySelectorAll(".toggle-eye-container");
+const input = form.querySelectorAll("input");
+const password = document.querySelector("#user-password");
+const confirmPassword = document.querySelector("#confirm-user-password");
+
+function validateEmail(email) {
+  const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]/;
+  return pattern.test(email);
+}
+
+function addInputCaution(eTarget, msg) {
+  eTarget.classList.add("input-focus-out");
+  const span = document.createElement("span");
+  span.textContent = msg;
+  span.classList.add("caution");
+  if (eTarget.nextElementSibling.classList.contains("caution")) {
+    eTarget.nextElementSibling.remove();
+    eTarget.after(span);
+  } else {
+    eTarget.after(span);
+  }
+
+  eTarget.classList.add("caution-margin");
+}
+
+function removeInputCaution(eTarget) {
+  if (eTarget.nextElementSibling.classList.contains("caution")) {
+    eTarget.nextElementSibling.remove();
+    eTarget.classList.remove("caution-margin");
+  }
+
+  eTarget.classList.remove("input-focus-out");
+}
+
+function btnActivation() {
+  const temp = [];
+  for (i = 0; i < input.length; i++) {
+    temp[i] = input[i].value === "";
+  }
+  switch (input.length) {
+    case 2:
+      if (
+        !temp.includes(true) &&
+        validateEmail(input[0].value) &&
+        !(input[1].value.length < 8)
+      ) {
+        form.querySelector(".login-page-btn").removeAttribute("disabled");
+      } else {
+        form.querySelector(".login-page-btn").setAttribute("disabled", true);
+      }
+
+      break;
+    case 4:
+      if (
+        !temp.includes(true) &&
+        validateEmail(input[0].value) &&
+        !(input[2].value.length < 8) &&
+        input[3].value === password.value
+      ) {
+        form.querySelector(".login-page-btn").removeAttribute("disabled");
+      } else {
+        form.querySelector(".login-page-btn").setAttribute("disabled", true);
+      }
+      break;
+  }
+}
+
+function cautionFocusOutEvent(e) {
+  if (e.target.classList.contains("user-input")) {
+    switch (e.target.id) {
+      case "user-email":
+        if (e.target.value === "") {
+          addInputCaution(e.target, "이메일을 입력해주세요.");
+        } else if (!validateEmail(e.target.value)) {
+          addInputCaution(e.target, "잘못된 이메일 형식입니다.");
+        } else {
+          removeInputCaution(e.target);
+        }
+        break;
+
+      case "user-password":
+        if (e.target.value === "") {
+          addInputCaution(e.target, "비밀번호를 입력해주세요.");
+        } else if (e.target.value.length < 8) {
+          addInputCaution(e.target, "비밀번호를 8자 이상 입력해주세요.");
+        } else {
+          removeInputCaution(e.target);
+        }
+        if (confirmPassword !== null) {
+          if (
+            password.value !== confirmPassword.value &&
+            confirmPassword.value !== ""
+          ) {
+            addInputCaution(confirmPassword, "비밀번호가 일치하지 않습니다.");
+          } else {
+            removeInputCaution(confirmPassword);
+          }
+        }
+        break;
+
+      case "nickname":
+        if (e.target.value === "") {
+          addInputCaution(e.target, "닉네임을 입력해주세요.");
+        } else {
+          removeInputCaution(e.target);
+        }
+        break;
+
+      case "confirm-user-password":
+        if (e.target.value !== password.value && confirmPassword.value !== "") {
+          addInputCaution(e.target, "비밀번호가 일치하지 않습니다.");
+        } else {
+          removeInputCaution(e.target);
+        }
+
+        break;
+    }
+  }
+  btnActivation();
+}
+
+function cautionInputEvent(e) {
+  if (e.target.classList.contains("user-input")) {
+    switch (e.target.id) {
+      case "user-email":
+        if (e.target.value !== "" && validateEmail(e.target.value)) {
+          removeInputCaution(e.target);
+        }
+        break;
+
+      case "user-password":
+        if (e.target.value !== "" && !(e.target.value.length < 8)) {
+          removeInputCaution(e.target);
+        }
+        if (confirmPassword !== null) {
+          if (
+            confirmPassword.value !== password.value &&
+            confirmPassword.value !== ""
+          ) {
+            addInputCaution(confirmPassword, "비밀번호가 일치하지 않습니다.");
+          } else if (confirmPassword.value === password.value) {
+            removeInputCaution(confirmPassword);
+          }
+        }
+        break;
+
+      case "nickname":
+        if (e.target.value !== "") {
+          removeInputCaution(e.target);
+        }
+        break;
+
+      case "confirm-user-password":
+        if (confirmPassword.value === password.value) {
+          removeInputCaution(e.target);
+        }
+
+        break;
+    }
+  }
+  btnActivation();
+}
+
+form.addEventListener("focusout", cautionFocusOutEvent);
+form.addEventListener("input", cautionInputEvent);
