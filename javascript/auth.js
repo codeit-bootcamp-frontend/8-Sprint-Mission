88a4769e27cbac1cb9a05632321a1524d1ca726f
seunghyun <tmdgus7048@naver.com>
2024-06-12 21:02:39 +0900

    email input event 추가 email input focus out시 에러 확인후 스타일 변화

--- javascript/auth.js
+++ javascript/auth.js
@@ -1,6 +1,7 @@
 const passwordToggleButton = document.querySelectorAll(
   ".password-visible-toggle-btn"
 );
+const emailInput = document.getElementById("auth-email");
 
 const setToggleButtonState = (toggleButton, isPasswordVisible) => {
   const toggleImg = toggleButton.querySelector("img");
@@ -25,6 +26,32 @@ const handlePasswordToggleButton = (e) => {
   setToggleButtonState(toggleButton, isPasswordVisible);
 };
 
+const handleEmailValidation = () => {
+  const emptyEmail = document.querySelector("#email-empty");
+  const errorEmail = document.querySelector("#email-error");
+  const emailValue = emailInput.value;
+  const regex =
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+  if (!emailValue) {
+    errorEmail.style.display = "none";
+    emptyEmail.style.display = "block";
+    emailInput.style.border = "1px solid #f74747";
+    return;
+  }
+  emptyEmail.style.display = "none";
+  emailInput.style.border = "1px solid none";
+  if (regex.test(emailValue)) {
+    errorEmail.style.display = "none";
+    emailInput.style.border = "1px solid none";
+    return;
+  }
+  errorEmail.style.display = "block";
+  emailInput.style.border = "1px solid #f74747";
+};
+
 passwordToggleButton.forEach((passwordBtn) => {
   passwordBtn.addEventListener("click", handlePasswordToggleButton);
 });
+
+emailInput.addEventListener("focusout", handleEmailValidation);
