5e37688f94a46ca4fad562ed904f1f0f4381734d
L1m3Kun <sungku2757@gmail.com>
2024-06-13 11:04:38 +0900

    feat: 로그인 페이지 검증  로직 구현 1. 이메일 형식 검증 2. 비밀번호 형식 검증 3. 이메일, 비밀번호 검증에 따른 로그인 버튼 활성화

---
+++ modules/validations.js
@@ -0,0 +1,26 @@
+const isValidEmail = (inputText) => {
+    if (inputText === '') {
+        return { isvaild: false, message: "empty" };
+    }
+    
+    const emailRegex = new RegExp(/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i);
+    
+    if ( !emailRegex.test(inputText) ){
+        return { isvaild: false, message: "invalid" };
+    }
+    return { isvaild: true, message: "vaild" };   
+}
+
+const isValidPassword = (inputText) => {
+    if (!inputText.length) {
+        return { isvaild: false, message: "empty" };
+    }
+
+    if (inputText.length < 8) {
+        return { isvaild: false, message: "less length" };
+    }
+    return { isvaild: true, message:"vaild" };
+}
+
+
+export { isValidEmail, isValidPassword }
