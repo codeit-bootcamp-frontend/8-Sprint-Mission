66e63dab17b274946e4d02f87ea6a2debe2bae8b
L1m3Kun <sungku2757@gmail.com>
2024-06-13 17:00:34 +0900

    chore: 변수명 오타 수정 vaild -> valid

--- modules/validations.js
+++ modules/validations.js
@@ -1,41 +1,41 @@
 const isValidEmail = (inputText) => {
     if (inputText === '') {
-        return { isvaild: false, message: "empty" };
+        return { isvalid: false, message: "empty" };
     }
     
     const emailRegex = new RegExp(/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i);
     
     if ( !emailRegex.test(inputText) ){
-        return { isvaild: false, message: "invalid" };
+        return { isvalid: false, message: "invalid" };
     }
-    return { isvaild: true, message: "vaild" };   
+    return { isvalid: true, message: "valid" };   
 }
 
 const isValidNickname = (inputText) => {
     if (inputText === '') {
-        return { isvaild: false, message: "empty" };
+        return { isvalid: false, message: "empty" };
     }
     
-    return { isvaild: true, message: "vaild" };   
+    return { isvalid: true, message: "valid" };   
 }
 
 const isValidPassword = (inputText) => {
     if (!inputText.length) {
-        return { isvaild: false, message: "empty" };
+        return { isvalid: false, message: "empty" };
     }
 
     if (inputText.length < 8) {
-        return { isvaild: false, message: "less length" };
+        return { isvalid: false, message: "less length" };
     }
-    return { isvaild: true, message:"vaild" };
+    return { isvalid: true, message:"valid" };
 }
 
 const isValidPasswordConfirm = (inputText, password) => {
     if (inputText !== password) {
-        return { isvaild: false, message: "differ" };
+        return { isvalid: false, message: "differ" };
     }
 
-    return { isvaild: true, message:"vaild" };
+    return { isvalid: true, message:"valid" };
 }
 
 
