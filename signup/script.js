6ccf4f3d963230948017bd1a1a22a386b326124c
Jeongmin Oh <ojm51@naver.com>
2024-06-13 15:41:42 +0900

    feat(login, signup): input 값이 모두 유효할 때  버튼이 활성화되는 기능 추가

--- signup/script.js
+++ signup/script.js
@@ -1,58 +1,90 @@
 const inputEmail = document.getElementById("input-email");
 const errorMsgEmail = document.querySelector(".error-message.email");
+let flagEmail = 0;
 inputEmail.addEventListener("focusout", ({ target }) => {
   const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
   if (target.value === "") {
     target.classList.add("error");
     errorMsgEmail.style.display = "block";
+    flagEmail = 0;
   } else if (!pattern.test(target.value)) {
     target.classList.add("error");
     errorMsgEmail.style.display = "block";
     errorMsgEmail.textContent = "잘못된 이메일 형식입니다.";
+    flagEmail = 0;
   } else {
     target.classList.remove("error");
     errorMsgEmail.style.display = "none";
+    flagEmail = 1;
   }
+  checkInputs();
 });
 
 const inputNickname = document.getElementById("input-nickname");
 const errorMsgNickname = document.querySelector(".error-message.nickname");
+let flagNickname = 0;
 inputNickname.addEventListener("focusout", ({ target }) => {
   if (target.value === "") {
     target.classList.add("error");
     errorMsgNickname.style.display = "block";
+    flagNickname = 0;
   } else {
     target.classList.remove("error");
     errorMsgNickname.style.display = "none";
+    flagNickname = 1;
   }
+  checkInputs();
 });
 
 const inputPassword = document.getElementById("input-password");
 const errorMsgPassword = document.querySelector(".error-message.password");
+let flagPassword = 0;
 inputPassword.addEventListener("focusout", ({ target }) => {
   if (target.value === "") {
     target.classList.add("error");
     errorMsgPassword.style.display = "block";
+    flagPassword = 0;
   } else if (target.value.length < 8) {
     target.classList.add("error");
     errorMsgPassword.style.display = "block";
     errorMsgPassword.textContent = "비밀번호를 8자 이상 입력해주세요.";
+    flagPassword = 0;
   } else {
     target.classList.remove("error");
     errorMsgPassword.style.display = "none";
+    flagPassword = 1;
   }
+  checkInputs();
 });
 
 const inputPasswordCheck = document.getElementById("input-password-check");
 const errorMsgPasswordCheck = document.querySelector(
   ".error-message.password-check"
 );
+let flagPasswordCheck = 0;
 inputPasswordCheck.addEventListener("focusout", ({ target }) => {
   if (target.value !== inputPassword.value) {
     target.classList.add("error");
     errorMsgPasswordCheck.style.display = "block";
+    flagPasswordCheck = 0;
   } else {
     target.classList.remove("error");
     errorMsgPasswordCheck.style.display = "none";
+    flagPasswordCheck = 1;
   }
+  checkInputs();
 });
+
+const signupBtn = document.querySelector(".form-signup-btn");
+function checkInputs() {
+  if (flagEmail && flagNickname && flagPassword && flagPasswordCheck) {
+    signupBtn.disabled = false;
+    signupBtn.classList.add("enabled");
+    signupBtn.addEventListener("click", () => {
+      window.location.href = "../signup/index.html";
+    });
+  } else {
+    signupBtn.disabled = true;
+    signupBtn.classList.remove("enabled");
+  }
+}
