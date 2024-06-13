6aa4fdb35484ed95cceba5a83667e64d46bc2589
손재헌 <jingmsi014@gmail.com>
2024-06-13 22:45:48 +0900

    Change <form> into <div> with 'pseudo-form' class
    - Changed the <form> tag in login and signup htmls into <div> with
      'pseudo-form' class to make the button work as intended.

--- scripts/constLib.js
+++ scripts/constLib.js
@@ -1,6 +1,6 @@
-const form = document.querySelector('form');
+const form = document.querySelector('.pseudo-form');
 const inputs = document.querySelectorAll('input');
-const btn = document.querySelector('.doit');
+const btn = document.querySelector('.action-button');
 
 const emailInput = document.querySelector('#email');
 const nicknameInput = document.querySelector('#nickname');
@@ -10,4 +10,7 @@ const verifyPasswordInput = document.querySelector('#verify-password');
 const loginPage = '/login';
 const itemsPage = '/items';
 
-export { form, inputs, btn, emailInput, nicknameInput, passwordInput, verifyPasswordInput, loginPage, itemsPage };
+const loginButtonLink = '/items';
+const signupButtonLink = '/signup';
+
+export { form, inputs, btn, emailInput, nicknameInput, passwordInput, verifyPasswordInput, loginPage, itemsPage, loginButtonLink, signupButtonLink };
--- scripts/signupEventHandler.js
+++ scripts/signupEventHandler.js
@@ -2,22 +2,30 @@
 
 import toggleVisibility from "./togglePasswordVisibility.js";
 import btnValidCheck from "./buttonValidationCheck.js";
-import { form, btn } from "./constLib.js";
+import { form, btn, loginButtonLink, signupButtonLink } from "./constLib.js";
 import emailValidationCheck from "./emailValidationCheck.js";
 import nicknameEmptyCheck from "./nicknameEmptyCheck.js";
 import passwordVerifiedCheck from "./passwordVerifiedCheck.js";
 import passwordFormatCheck from "./passwordFormatCheck.js";
+import { btnLink } from "./stdLib.js";
 
 const passwordVisibilityIcons = document.querySelectorAll('.password-visibility-icon');
 
 
-// 버튼 disabled 설정
+// 버튼 disabled 설정 - 페이지 진입 후 아무 동작도 하지 않았을때 버튼이 동작하는걸 방지합니다.
 btn.disabled = true;
+
+
+// 버튼 링크 설정
+if (btn.classList.contains('login')) btn.addEventListener('click', () => { btnLink(loginButtonLink) });
+else if (btn.classList.contains('signup')) btn.addEventListener('click', () => { btnLink(signupButtonLink) });
+
 
 // 눈 이미지에 패스워드 가시성 토글 스크립트 지정
 for (let each of passwordVisibilityIcons) {
     each.addEventListener('click', toggleVisibility);
 }
+
 
 // 폼에 각 이벤트 지정
 
