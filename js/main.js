fba853c818f629b364b04332a70e87e8918bc896
leehj322 <gudwns8956@naver.com>
2024-06-14 15:46:54 +0900

    feat(login/signup): Add password visiblitiy toggle

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 import { emailInputCheck,nicknameInputCheck, pwInputCheck, pwRetypeInputCheck } from './input.js';
 import { buttonDisableChecker } from './button.js';
+import { pwVisibleToggle } from './pw_visibility.js';
 
 const emailInput = document.querySelector('#email-input');
 const pwInput = document.querySelector('#password-input');
 const pwRetypeInput = document.querySelector('#password-retype-input');
 const nicknameInput = document.querySelector('#nickname-input');
+
+const pwVisibleBtn = document.querySelector('#password-visibility-button');
+const pwRetypeVisibleBtn = document.querySelector('#password-retype-visibility-button');
 
 // login page event
 emailInput.addEventListener('focusout', emailInputCheck);
@@ -13,12 +17,16 @@ pwInput.addEventListener('focusout', pwInputCheck);
 emailInput.addEventListener('focusout', buttonDisableChecker);
 pwInput.addEventListener('focusout', buttonDisableChecker);
 
+pwVisibleBtn.addEventListener('click', pwVisibleToggle);
 
-// signup page event
+
+// additional event for signup page
 if (pwRetypeInput && nicknameInput) {
   nicknameInput.addEventListener('focusout', nicknameInputCheck);
   pwRetypeInput.addEventListener('focusout', pwRetypeInputCheck);
 
   nicknameInput.addEventListener('focusout', buttonDisableChecker);
   pwRetypeInput.addEventListener('focusout', buttonDisableChecker);
+
+  pwRetypeVisibleBtn.addEventListener('click', pwVisibleToggle);
 }
