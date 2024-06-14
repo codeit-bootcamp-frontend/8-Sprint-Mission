95bdacb1be9ee669c0fa0a5ca9d82901065d27f2
marie1016 <kjo5281@gmail.com>
2024-06-14 21:07:22 +0900

    스프린트 미션4

---
+++ JS/login.js
@@ -0,0 +1,36 @@
+import {
+  email,
+  input,
+  emailCheck,
+  password,
+  passwordCheck,
+  eyeOff,
+  eye,
+  btn,
+} from "./signup.js";
+
+email.addEventListener("focusout", emailCheck);
+
+password.addEventListener("focusout", passwordCheck);
+
+eyeOff.addEventListener("click", eye);
+
+//버튼 활성화/비활성화
+
+function loginValid() {
+  if (
+    input.value !== "" &&
+    /\S+@\S+\.\S+/.test(email.value) === true &&
+    password.value.length >= 8
+  ) {
+    btn.disabled = false;
+    btn.style.backgroundColor = "#3692ff";
+    btn.addEventListener("click", () => {
+      window.location.href = "/items.html";
+    });
+  } else {
+    btn.disabled = true;
+  }
+}
+
+loginBtn.addEventListener("click", loginValid);
