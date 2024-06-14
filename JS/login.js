de79eb3591ebe0240a2d704e2e845a571ce3220f
nulllife2510 <122213489+nulllife2510@users.noreply.github.com>
2024-06-15 02:57:22 +0900

    add(login/signup): 모든 required input에 대한 에러 메세지 출력 기능 완성

--- JS/login.js
+++ JS/login.js
@@ -1,40 +1,94 @@
-const FORM_GROUP = document.querySelector(".form-group");
+// window.onload = function () {
+//   (function () {
+//     document.getElementsByClassName("login-button").disabled = true;
+//   })();
+// };
+
+const FORM_ELEMENT = document.querySelector("form");
 
 /**
  * 알고리즘:
- * focusout 이벤트가 발생하면 두 가지를 체크:
- *
- * 1. 값이 비었는가?
- *    - 값이 비었으면 에러 메시지 및 테두리를 표시
- *    - 값이 있으면 2단계로 이동
- * 2. 값이 있으면: 이메일 형식에 맞는가?
- *    - 형식에 맞으면 에러 메시지 제거 및 테두리 제거
- *    - 형식에 맞지 않으면 에러 메시지를 표시
  */
-FORM_GROUP.addEventListener("focusout", (e) => {
+FORM_ELEMENT.addEventListener("focusout", (e) => {
   if (e.target.classList.contains("form-input")) {
-    const errorMessage =
-      e.target.parentElement.querySelector(".form-input-errmsg");
-
-    if (e.target.value == "") {
-      e.target.style.border = "1px solid var(--error)";
-      errorMessage
-        ? (errorMessage.textContent = "이메일을 입력해주세요.")
-        : createErrorMessage(e.target, "이메일을 입력해주세요.");
-    } else if (validateEmail(e.target.value)) {
-      errorMessage && errorMessage.remove();
-      e.target.style.border = "";
-    } else {
-      errorMessage
-        ? (errorMessage.textContent = "잘못된 이메일 형식입니다.")
-        : createErrorMessage(e.target, "잘못된 이메일 형식입니다.");
-    }
+    validateInput(e.target);
   }
 });
 
+/**
+ * Input 태그를 검사하는 함수
+ * @param {HTMLElement} input - focusout이 일어났을 때 검사할 Input
+ */
+function validateInput(input) {
+  const errorMessageElement =
+    input.parentElement.querySelector(".form-input-errmsg");
+  let isValid = false; // boolean (input 태그가 유효한지 검증하는 변수)
+  let validMessage = ""; // 에러 메세지
+
+  if (input.value == "") {
+    validMessage = `${input.dataset.input}을(를) 입력해주세요.`;
+  } else {
+    switch (input.id) {
+      case "email":
+        isValid = validateEmail(input.value);
+        validMessage = isValid ? "" : "잘못된 이메일 형식입니다.";
+        break;
+
+      case "password":
+        isValid = validatePassword(input.value);
+        validMessage = isValid ? "" : "비밀번호를 8자 이상 입력해주세요.";
+        break;
+
+      case "password-check":
+        const firstPassword = document.querySelector("#password");
+        isValid = validatePasswordCheck(firstPassword.value, input.value);
+        validMessage = isValid ? "" : "비밀번호가 일치하지 않습니다.";
+        break;
+
+      default:
+        isValid = true;
+    }
+  }
+
+  if (!isValid) {
+    input.style.border = "1px solid var(--error)";
+    if (errorMessageElement) {
+      errorMessageElement.textContent = validMessage;
+    } else {
+      createErrorMessage(input, validMessage);
+    }
+  } else {
+    input.style.border = "";
+    errorMessageElement && errorMessageElement.remove();
+  }
+}
+
+/**
+ * 이메일이 정규식에 맞는지 검증하는 함수
+ * @param {string} email - 이메일
+ * @return {boolean} - 유효하면 true, 않으면 false
+ */
 function validateEmail(email) {
   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return re.test(email);
+}
+
+/**
+ * 비밀번호의 길이가 맞는지 확인하는 함수
+ * @param {string} password - 확인할 비밀번호
+ * @return {boolean} - 유효하면 true, 않으면 false
+ */
+function validatePassword(password) {
+  return password.length <= 8 ? false : true;
+}
+
+/**
+ * 비밀번호 확인 input과 비밀번호 input이 같은지 확인하는 함수
+ * @param {string} pw1 - 비교할 비밀번호
+ * @param {string} pw2 - 비밀번호 확인
+ */
+function validatePasswordCheck(pw1, pw2) {
+  return pw1 === pw2;
 }
 
 /**
