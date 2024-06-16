5136d8d2395daf7a943bd96c34e3d0df73ee7f43
nayoonju <bubblenana@bubblecon.io>
2024-06-17 03:03:01 +0900

    fix: signup 수정

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,67 +1,25 @@
 import {
   form,
-  email,
-  password,
-  confirmPassword,
-  nickName,
-  validateEmail,
-  validateNickname,
-  validatePassword,
-  validateConfirmPassword,
-  checkFormValidity,
-  submitBtn
+  inputList,
+  checkInputs,
+  checkAllInput,
 } from './form-validation.js';
-import { passwordToggleBtns, passwordToggle } from './password-toggle.js';
 
-submitBtn.disabled = true;
-
-
-email.addEventListener('blur', function () {
-  this.parentNode.classList.add('is-valid');
-  validateEmail();
-});
-nickName.addEventListener('blur', function () {
-  this.parentNode.classList.add('is-valid');
-  validateNickname();
-});
-password.addEventListener('blur', function () {
-  this.parentNode.classList.add('is-valid');
-  validatePassword();
-});
-confirmPassword.addEventListener('blur', function () {
-  this.parentNode.classList.add('is-valid');
-  validateConfirmPassword();
+inputList.forEach((input) => {
+  input.addEventListener('blur', function () {
+    this.parentNode.classList.add('is-error');
+    checkInputs(input);
+  });
+  input.addEventListener('keyup', () => {
+    checkInputs(input);
+    checkAllInput();
+  })
 });
 
-email.addEventListener('keyup', function () {
-  if (this.parentNode.classList.contains('is-valid')) {
-  }
-  validateEmail();
-  checkFormValidity();
-});
-nickName.addEventListener('keyup', function () {
-  if (this.parentNode.classList.contains('is-valid')) {
-  }
-  validateNickname();
-  checkFormValidity();
-});
-password.addEventListener('keyup', function () {
-  if (this.parentNode.classList.contains('is-valid')) {
-  }
-  validatePassword();
-  checkFormValidity();
-});
-confirmPassword.addEventListener('keyup', function () {
-  if (this.parentNode.classList.contains('is-valid')) {
-  }
-  validateConfirmPassword();
-  checkFormValidity();
-});
-
-
+// form 제출
 form.addEventListener('submit', function (event) {
-  if (validateEmail() && validatePassword() && validateConfirmPassword() && validateNickname()) {
-    event.preventDefault();
+  event.preventDefault();
+  if (checkAllInput) {
     alert('회원가입성공');
     window.location.href = '/signup.html';
   }
@@ -69,6 +27,26 @@ form.addEventListener('submit', function (event) {
 
 
 // 패스워드 보기
+function passwordToggle(e) {
+  let button = e.currentTarget;
+  let passwordInput = button.parentElement.querySelector('input');
+  let toggleIcon = document.createElement('i');
+  toggleIcon.classList.add('icon');
+
+  const shouldShowPassword = passwordInput.type === 'password';
+  passwordInput.type = shouldShowPassword ? 'text' : 'password';
+
+  toggleIcon.classList.toggle('ic_visible_on', !shouldShowPassword);
+  toggleIcon.classList.toggle('ic_visible_off', shouldShowPassword);
+
+  const ariaLabel = shouldShowPassword ? '비밀번호 숨기기' : '비밀번호 보기';
+  this.setAttribute("aria-label", ariaLabel);
+
+  button.innerHTML = '';
+  button.appendChild(toggleIcon);
+}
+
+const passwordToggleBtns = document.querySelectorAll('.password-toggle-button');
 passwordToggleBtns.forEach(btn => {
   btn.addEventListener('click', passwordToggle);
 });
