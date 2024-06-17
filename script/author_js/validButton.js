ea4b7bc36ed702f01786043903807044376be454
iclock1960 <iclock1960@naver.com>
2024-06-18 01:39:42 +0900

    리팩토링: vaildButton.js button style 적용 방법 개선 (add & remove > toggle)

--- script/author_js/validButton.js
+++ script/author_js/validButton.js
@@ -2,14 +2,16 @@ import { checkValid } from "./checkValid.js";
 
 export function validButton(button, inputs) {
 
-  const isAllInputValid = [...input].every(input => checkValid(input));
-  
+  const isAllInputValid = [...inputs].every(input => checkValid(input));
+  button.classList.toggle('enaled', isAllInputValid)
+
+  console.log(isAllInputValid);
   if(isAllInputValid){
-    button.classList.add('enabled')
+    // button.classList.add('enabled')
     button.disabled = false;
     console.log(button.disabled);
   }else{
-    button.classList.remove('enabled')
+    // button.classList.remove('enabled')
     button.disabled = true;
     console.log(button.disabled);
   }
