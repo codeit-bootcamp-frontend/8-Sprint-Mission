89a21d93cc08b062047be8275eb7b9d6ed07850a
iclock1960 <iclock1960@naver.com>
2024-06-17 23:37:48 +0900

    리펙토링: 가독성 차원에서 mouseManager.js > buttonManager.js 로 변경

--- script/author_js/validButton.js
+++ script/author_js/validButton.js
@@ -1,25 +1,19 @@
+import { checkValid } from "./checkValid";
+
 export function validButton(button, inputs) {
 
-  console.log('이벤트핸들러 진입');
-  console.log({button});
-  console.log({inputs});
   let number = 0;
 
   for(let input of inputs){
-    console.log('포문진입');
-    if(input.checkValidity()){
-      console.log('넘버 +1');
-      number++
+    if(checkValid(valid)){
+      number++;
     }
   }
-
   if(number === inputs.length){
-    console.log('number === inputs.length')
     button.classList.add('enabled')
     button.disabled = false;
     console.log(button.disabled);
   }else{
-    console.log('else')
     button.classList.remove('enabled')
     button.disabled = true;
     console.log(button.disabled);
