70bf5f3ea3f3e897c2f067cd404ebee13e47458d
Injaeeee <97098671+Injaeeee@users.noreply.github.com>
2024-06-18 22:39:06 +0900

    topbar

---
+++ src/Topbar.js
@@ -0,0 +1,15 @@
+
+function Topbar() {
+
+    return (<header className="topbar">
+        <a href="/" className="logo">
+            <img src="" />
+            <img src="" />
+        </a>
+        <a className="login" href="login/">로그인</a>
+
+    </header>);
+
+}
+
+export default Topbar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom/client';
+import Topbar from './Topbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
+
+    <Topbar></Topbar>
     <h1>안녕 리액트!</h1>
   </>
 );
