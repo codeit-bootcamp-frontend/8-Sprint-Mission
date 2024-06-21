677c78529311dd54cef23ec2e203a70e4712e50d
최원혁 <mangoj425@gmail.com>
2024-06-21 11:08:14 +0900

    과제 시작

---
+++ src/component/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Fragment } from "react";
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>ㅋㅋ</h1>
+    </Fragment>
+  );
+};
+
+export default App;
