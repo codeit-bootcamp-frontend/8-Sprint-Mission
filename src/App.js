f8687d7c4c1203f6661d5d23336ac57a48702d07
seunghyun <tmdgus7048@naver.com>
2024-06-21 23:11:28 +0900

    appjs에 라우터 페이지 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MarketPage from "./pages/MarketPage/MarketPage";
+import HomePage from "./pages/HomePage/HomePage";
+import Header from "./components/HeaderComponent/Header";
+import AddItemPage from "./pages/AddItemPage/AddItemPage";
 
 function App() {
   return (
-    <div className="App">
-      <MarketPage />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/items" element={<MarketPage />} />
+        <Route path="/addItem" element={<AddItemPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
