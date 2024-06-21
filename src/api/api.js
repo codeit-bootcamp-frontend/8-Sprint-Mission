56fe55ca2ec03d73e2b92592c026cff03c6cb622
seunghyun <tmdgus7048@naver.com>
2024-06-21 23:12:57 +0900

    products api

---
+++ src/api/api.js
@@ -0,0 +1,17 @@
+export async function getProducts(
+  pageSize = 10,
+  orderBy = "favorite",
+  page = 1,
+  keyword = ""
+) {
+  const query = `pageSize=${pageSize}&orderBy=${orderBy}`;
+  try {
+    const response = await fetch(
+      `https://panda-market-api.vercel.app/products?${query}`
+    );
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
