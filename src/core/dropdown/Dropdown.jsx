89297c96577abe894bf7bc79a12770e3d425bc1b
L1m3Kun <sungku2757@gmail.com>
2024-06-22 03:21:51 +0900

    feat: dropdown 반응형 추가

--- src/core/dropdown/Dropdown.jsx
+++ src/core/dropdown/Dropdown.jsx
@@ -1,21 +1,45 @@
 import { useState } from "react";
+import useResize from "../../lib/hooks/useResize";
 
 import ArrowDown from "../assets/icons/dropdown/arrow_down.svg";
+import DropdownIcon from "../assets/icons/dropdown/down-icon.svg";
+
 import DropdownMenu from "./DropdownMenu";
 
 const Dropdown = ({ isLoading, order, handleListClick }) => {
+  const getPageSize = (width) => {
+    if (width < 768) {
+      return 1;
+    }
+    return 0;
+  };
+  const size = useResize({ getPageSize });
   const [isDrop, setIsDrop] = useState(false);
   const handleDropdown = () => {
+    setIsDrop(!isDrop);
+  };
+
+  const handleOrderClick = (e) => {
+    handleListClick(e);
     setIsDrop(!isDrop);
   };
   return (
     <div className="dropdown-container">
       <button className="dropdown-btn" onClick={handleDropdown}>
-        <span>{order === "recent" ? "최신순" : "좋아요순"}</span>
-        <img src={ArrowDown} alt="드랍다운 아이콘" />
+        {size ? (
+          <img src={DropdownIcon} alt="드랍다운 아이콘" />
+        ) : (
+          <div>
+            <span>{order === "recent" ? "최신순" : "좋아요순"}</span>
+            <img src={ArrowDown} alt="드랍다운 아이콘" />
+          </div>
+        )}
       </button>
       {isDrop && (
-        <DropdownMenu isLoading={isLoading} handleListClick={handleListClick} />
+        <DropdownMenu
+          isLoading={isLoading}
+          handleListClick={handleOrderClick}
+        />
       )}
     </div>
   );
