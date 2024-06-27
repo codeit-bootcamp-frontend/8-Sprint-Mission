c9f0afa2b2cb253fec5b017beb344dab71ba7406
yamdang <youm4000@naver.com>
2024-06-27 17:35:10 +0900

    base URL 환경변수 생성

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
 export async function getProducts({ page, pageSize, order }) {
+  const apiUrl = process.env.REACT_APP_API_URL;
   const query = `page=${page}&pageSize=${pageSize}&orderBy=${order}`;
-  const response = await fetch(
-    `https://panda-market-api.vercel.app/products?${query}`
-  );
+  const response = await fetch(`${apiUrl}.vercel.app/products?${query}`);
   const body = await response.json();
   return body;
 }
