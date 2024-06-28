51d81467d8774ab1a8c3923303ebf9459a9dc44b
jaewanLee <wodhks3791@naver.com>
2024-06-28 22:14:20 +0900

    feat(AddItem): 상품 이미지 미리보기 적용, 반응형 웹 구현

--- src/pages/AddItemPage/components/FileInput.js
+++ src/pages/AddItemPage/components/FileInput.js
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import placeholderImg from "../../../assets/images/icons/preview-placeholder.png";
+import registerImg from "../../../assets/images/icons/ic_register.png";
 import resetImg from "../../../assets/images/icons/ic_reset.png";
 import "./FileInput.css";
 
@@ -33,23 +33,32 @@ function FileInput({ className = "", name, value, initialPreview, onChange }) {
 
   return (
     <div className={`FileInput ${className}`}>
-      <img
-        className={`FileInput-preview ${preview ? "selected" : ""}`}
-        src={preview || placeholderImg}
-        alt="이미지 미리보기"
-      />
-      <input
-        className="FileInput-hidden-overlay"
-        type="file"
-        accept="image/png, image/jpeg"
-        onChange={handleChange}
-        ref={inputRef}
-      />
-      {value && (
-        <button className="FileInput-clear-button" onClick={handleClearClick}>
-          <img src={resetImg} alt="선택해제" />
-        </button>
-      )}
+      <div className="FileInput-input-wrapper">
+        <input
+          className="FileInput-hidden-overlay"
+          type="file"
+          accept="image/png, image/jpeg"
+          onChange={handleChange}
+          ref={inputRef}
+        />
+        <img
+          className="FileInput-register-icon"
+          src={registerImg}
+          alt="이미지 등록"
+        />
+      </div>
+      <div className="Fileinput-preview-wrapper">
+        <img
+          className="FileInput-preview"
+          src={preview}
+          alt="이미지 미리보기"
+        />
+        {value && (
+          <button className="FileInput-clear-button" onClick={handleClearClick}>
+            <img src={resetImg} alt="선택해제" />
+          </button>
+        )}
+      </div>
     </div>
   );
 }
