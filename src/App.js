710e6579b70fc1ccb93ed835f8d4fe5583097461
kzlsk <rlaflsk4887@gmail.com>
2024-06-28 23:36:38 +0900

    header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Commu from "./pages/Commu";
+import Market from "./pages/Market";
+import Login from "./pages/Login";
+import Header from "./global/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+
+      <div>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="items" element={<Market />} />
+          <Route path="community" element={<Commu />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Commu.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Commu() {
+  return <div>Commu</div>;
+}
+
+export default Commu;
