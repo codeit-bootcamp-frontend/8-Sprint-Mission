45dc46952b4287123450fe2e9a9854a3d8d74488
junwankim <kjw0412c@gmail.com>
2024-06-30 23:44:07 +0900

    template내용 삭제 및 기본 틀 작성

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-import LoginPage from './pages/LoginPage/LoginPage.jsx';
-import MarketPage from './pages/MarketPage/MarketPage';
-import AddItemPage from './pages/AddItemPage/AddItemPage';
-import CommunityFeedPage from './pages/CommunityFeedPage/CommunityFeedPage';
-import Header from './components/Layout/Header';
+import Items from './Items.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
-      {/* Global Navigation Bar */}
-      <Header />
-
-      <div className="withHeader">
-        <Routes>
-          {/* React Router v6부터는 path="/" 대신 간단하게 `index`라고 표기하면 돼요 */}
-          <Route index element={<HomePage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="items" element={<MarketPage />} />
-          <Route path="additem" element={<AddItemPage />} />
-          <Route path="community" element={<CommunityFeedPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div>
+      <Items></Items>
+    </div>
   );
 }
 
---
+++ src/Items.jsx
@@ -0,0 +1,51 @@
+function Items() {
+  return (
+    <>
+      <div>
+        <button /> <button /> <button /> <button />
+      </div>
+      <div>
+        <div>베스트상품</div>
+        <div>
+          <div>
+            <img></img>
+            <div>아이패드 미니 팝니다.</div>
+            <div>500,000원</div>
+            <button />
+            <div>1000</div>
+          </div>
+          <div>
+            <img></img>
+            <div>아이패드 미니 팝니다.</div>
+            <div>500,000원</div>
+            <button />
+            <div>1000</div>
+          </div>
+          <div>
+            <img></img>
+            <div>아이패드 미니 팝니다.</div>
+            <div>500,000원</div>
+            <button />
+            <div>1000</div>
+          </div>
+          <div>
+            <img></img>
+            <div>아이패드 미니 팝니다.</div>
+            <div>500,000원</div>
+            <button />
+            <div>1000</div>
+          </div>
+        </div>
+        <div>
+          <div>판매중인 상품</div>
+          <form>
+            <input></input>
+            <button />
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Items;
