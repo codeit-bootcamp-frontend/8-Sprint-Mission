9f72df284b256e450548751b0a01aea2d83c8e3d
“choi-youngsun” <sunn7931@naver.com>
2024-07-01 01:04:15 +0900

    파일인풋 구현완료(이미지 미리보기 제외)

--- src/pages/AddItems/AddItemPage.js
+++ src/pages/AddItems/AddItemPage.js
@@ -1,29 +1,30 @@
 import "./AddItemPage.css";
+import FlieInput from "./FileInput";
 import { useState } from "react";
 
 function AddItem() {
-  const [name, setName] = useState("");
-  const [detail, setDetail] = useState("");
-  const [price, setPrice] = useState("");
-  const [tag, setTag] = useState("");
+  const [values, setValues] = useState({
+    name: "",
+    detail: "",
+    price: "",
+    tag: "",
+    imgFile: null,
+  });
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
+  const handleChange = (name, value) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
   };
-
-  const handleDetailChange = (e) => {
-    setDetail(e.target.value);
-  };
-  const handlePriceChange = (e) => {
-    setPrice(e.target.value);
-  };
-  const handleTagChange = (e) => {
-    setTag(e.target.value);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({ name, detail, price, tag });
+    console.log(values);
   };
 
   return (
@@ -31,6 +32,11 @@ function AddItem() {
       <form className="form-container" onSubmit={handleSubmit}>
         <h1>상품 등록하기</h1>
         <button>등록</button>
+        <FlieInput
+          name="imgFile"
+          value={values.imgFile}
+          onChange={handleChange}
+        />
         <label htmlFor="add-img">상품 이미지</label>
         <input
           id="add-img"
@@ -43,21 +49,24 @@ function AddItem() {
         <input
           id="item-name"
           name="item-name"
-          onChange={handleNameChange}
+          value={values.name}
+          onChange={handleInputChange}
           placeholder="상품명을 입력해주세요"
         ></input>
         <label htmlFor="item-detail">상품 소개</label>
         <textarea
           id="item-detail"
           name="item-detail"
-          onChange={handleDetailChange}
+          value={values.detail}
+          onChange={handleInputChange}
           placeholder="상품 소개를 입력해주세요"
         ></textarea>
         <label htmlFor="item-price">판매가격</label>
         <input
           id="item-price"
           name="item-price"
-          onChange={handlePriceChange}
+          value={values.price}
+          onChange={handleInputChange}
           type="number"
           placeholder="판매 가격을 입력해주세요"
         ></input>
@@ -65,7 +74,8 @@ function AddItem() {
         <input
           id="tag"
           name="tag"
-          onChange={handleTagChange}
+          value={values.tag}
+          onChange={handleInputChange}
           placeholder="태그를 입력해주세요"
         ></input>
       </form>
--- src/pages/AddItems/FileInput.js
+++ src/pages/AddItems/FileInput.js
@@ -1,8 +1,27 @@
-function FlieInput() {
+import { useRef } from "react";
+
+function FlieInput({ name, value, onChange }) {
+  const inputRef = useRef();
+
   const handleChange = (e) => {
-    console.log(e.target.files);
+    const nextValue = e.target.files[0];
+    onChange(name, nextValue);
   };
-  return <input type="file" onChange={handleChange} />;
+
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = "";
+    onChange(name, null);
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleChange} ref={inputRef} />;
+      {value && <button onClick={handleClearClick}>X</button>}
+    </div>
+  );
 }
 
 export default FlieInput;
