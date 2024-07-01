0a1ea5780aed518d13cab31842d48d31564f463e
leehj322 <gudwns8956@naver.com>
2024-07-01 12:29:50 +0900

    Build: Build previous commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,34 @@
+import { Link } from "react-router-dom";
+
+import logoWithTypoImgUrl from "../assets/logo_with_typo.png";
+import onlyTypoImgUrl from "../assets/logo_typo.png";
+
 function Login() {
-  return <h1>login page</h1>;
+  return (
+    <>
+      <Link to="/">
+        <div>
+          <img
+            className="header-logo-img"
+            src={logoWithTypoImgUrl}
+            alt="판다마켓 로고"
+          />
+          <img
+            className="header-typo-img"
+            src={onlyTypoImgUrl}
+            alt="판다마켓 로고"
+          />
+        </div>
+      </Link>
+      <form>
+        <h2></h2>
+        <input></input>
+        <input></input>
+        <button></button>
+      </form>
+      <div></div>
+    </>
+  );
 }
 
 export default Login;
