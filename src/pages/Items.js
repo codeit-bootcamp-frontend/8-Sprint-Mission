f2f46164d23bfbba0770d5f3b4469526cae35846
nayoonju <bubblenana@bubblecon.io>
2024-07-02 23:48:23 +0900

    fix: Item 구조 변경

--- src/pages/Items.js
+++ src/pages/Items.js
@@ -1,11 +1,24 @@
-import Header from './../layout/Header'; 
-import ItemMain from './../layout/ItemMain'; 
+import { Helmet } from 'react-helmet';
+import Header from './../layout/Header';
+import BestProductList from '../components/BestProductList';
+import AllProductList from '../components/AllProductList';
+import './Items.scss';
 
 function Items() {
   return (
     <>
-      <Header />  
-      <ItemMain />
+      <Helmet>
+        <title>판다마켓 - 중고마켓</title>
+      </Helmet>
+      <Header />
+      <main className="main-top">
+        <section className="product-wrap best">
+          <BestProductList />
+        </section>
+        <section className="product-wrap sale">
+          <AllProductList />
+        </section>
+      </main>
     </>
   );
 }
