83a03ecfecc278350be463af7fae107da26b096e
L1m3Kun <sungku2757@gmail.com>
2024-07-03 20:59:23 +0900

    feat: register 컴포넌트 추가

---
+++ src/components/home/RegisterInfo.tsx
@@ -0,0 +1,33 @@
+import {
+  Container,
+  ContainerCategory,
+  ContainerContent,
+  ContainerDescription,
+  ContainerTitle,
+} from "core/styles/Container";
+import { Section } from "core/styles/Section";
+import registerImg from "core/assets/images/main/register/registerImage.png";
+import { styled } from "styled-components";
+
+const RegisterContent = styled(ContainerContent)`
+  max-width: 32rem;
+`;
+
+const RegisterInfo = () => {
+  return (
+    <Section>
+      <Container>
+        <img src={registerImg} alt="Register Image" />
+        <RegisterContent>
+          <ContainerCategory>Register</ContainerCategory>
+          <ContainerTitle>판매를 원하는 상품을 등록하세요</ContainerTitle>
+          <ContainerDescription>
+            어떤 물건이든 판매하고 싶은 상품을 쉽게 등록하세요
+          </ContainerDescription>
+        </RegisterContent>
+      </Container>
+    </Section>
+  );
+};
+
+export default RegisterInfo;
