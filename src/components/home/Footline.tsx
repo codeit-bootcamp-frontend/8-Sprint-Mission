0b1efcfdcf81f4d7803137bda164bd0fcc788ed2
L1m3Kun <sungku2757@gmail.com>
2024-07-03 23:06:18 +0900

    feat: footline 컴포넌트 추가

--- src/components/home/Footline.tsx
+++ src/components/home/Footline.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import {
+  HeadFootLine,
+  HeadFootLineContent,
+  HeadFootLineTitle,
+} from "./HeadFootLine";
+
+import footlineImg from "core/assets/images/footline/footlineImage.png";
 
 const Footline = () => {
-  return <div></div>;
+  return (
+    <HeadFootLine>
+      <HeadFootLineContent>
+        <HeadFootLineTitle>
+          <h3>믿을 수 있는 판다마켓 중고거래</h3>
+        </HeadFootLineTitle>
+        <img src={footlineImg} alt="headline image" />
+      </HeadFootLineContent>
+    </HeadFootLine>
+  );
 };
 
 export default Footline;
