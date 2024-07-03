bf0127c21fe8f84d7bd425ca3af8b0b083490a2a
L1m3Kun <sungku2757@gmail.com>
2024-07-03 23:42:34 +0900

    feat: 메인 페이지 추가

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Nav from "./nav/Nav";
+import Home from "./home/Home";
 import Products from "./products/Products";
 import AddItem from "./addItem/AddItem";
 import Board from "./board/Board";
@@ -12,6 +13,7 @@ const App = () => {
       <GlobalStyles />
       <Nav />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/items" element={<Products />} />
         <Route path="/board" element={<Board />} />
         <Route path="/addItem" element={<AddItem />} />
---
+++ src/pages/home/Main.tsx
@@ -0,0 +1,15 @@
+import PopularInfo from "components/home/PopularInfo";
+import RegisterInfo from "components/home/RegisterInfo";
+import SearchInfo from "components/home/SearchInfo";
+
+const Main = () => {
+  return (
+    <main>
+      <PopularInfo />
+      <SearchInfo />
+      <RegisterInfo />
+    </main>
+  );
+};
+
+export default Main;
