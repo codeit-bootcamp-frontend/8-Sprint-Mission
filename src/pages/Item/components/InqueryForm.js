5222957416e3b0c73dceecaa5db4b1e09f66933e
nayoonju <bubblenana@bubblecon.io>
2024-07-04 20:33:35 +0900

    feat: InqueryForm.js 추가

---
+++ src/pages/Item/components/InqueryForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function InqueryForm({ newComment, setNewComment, handleSubmit }) {
+  const handleChange = event => {
+    setNewComment(event.target.value);
+  };
+  const isFormValid = newComment.trim() !== '';
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="input-group">
+        <label>문의하기</label>
+        <textarea
+          rows="3"
+          placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
+          value={newComment}
+          onChange={handleChange}></textarea>
+        <button type="submit" className="btn-sm btn-primary submit" disabled={!isFormValid}>
+          등록
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default InqueryForm;
