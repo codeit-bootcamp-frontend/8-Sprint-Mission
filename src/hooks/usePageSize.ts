1ac69d4cc46c368991246568bbe415b6b840cc7a
Minchan <98478661+itscold96@users.noreply.github.com>
2024-07-05 22:35:27 +0900

    fix(usePageSize):  오타 수정
    catagory -> category

--- src/hooks/usePageSize.ts
+++ src/hooks/usePageSize.ts
@@ -2,23 +2,23 @@ import useWindowSize from './useWindowSize';
 
 type ProductCategoryType = 'best' | 'forSale';
 
-const getPageSize = ({ catagory, innerWidth }: { catagory: ProductCategoryType; innerWidth: number }) => {
+const getPageSize = ({ category, innerWidth }: { category: ProductCategoryType; innerWidth: number }) => {
   if (innerWidth < 768) {
     // Mobile viewport
-    return catagory === 'best' ? 1 : 4;
+    return category === 'best' ? 1 : 4;
   } else if (innerWidth < 1280) {
     // Tablet viewport
-    return catagory === 'best' ? 2 : 6;
+    return category === 'best' ? 2 : 6;
   } else {
     // Desktop viewport
-    return catagory === 'best' ? 4 : 10;
+    return category === 'best' ? 4 : 10;
   }
 };
 
-const usePageSize = (catagory: ProductCategoryType): number => {
+const usePageSize = (category: ProductCategoryType): number => {
   const { innerWidth } = useWindowSize();
 
-  return getPageSize({ catagory, innerWidth });
+  return getPageSize({ category, innerWidth });
 };
 
 export default usePageSize;
