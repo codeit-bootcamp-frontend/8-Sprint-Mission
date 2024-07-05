54eefa0d202d0ba26f14e54ed0ab66867c33394e
seunghyun <tmdgus7048@naver.com>
2024-07-05 23:13:38 +0900

    product detail,comments get api추가

--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,52 @@
+const PRODUCTS_URL = "https://panda-market-api.vercel.app/products";
+
 export async function getProducts(
   pageSize = 0,
   orderBy = "favorite",
   page = 1,
   keyword = ""
 ) {
-  const query = `pageSize=${pageSize}&orderBy=${orderBy}`;
+  const query = `pageSize=${pageSize}&orderBy=${orderBy}&page=${page}&keyword=${keyword}`;
+  try {
+    const response = await fetch(`${PRODUCTS_URL}?${query}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    console.error("Failed to fetch products:", error);
+    throw error;
+  }
+}
+
+export async function getDetailProduct(productId) {
+  // throw new Error("error");
+  try {
+    const response = await fetch(`${PRODUCTS_URL}/${productId}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    console.error("fetch error:", error);
+    throw error;
+  }
+}
+
+export async function getComments(productId) {
   try {
     const response = await fetch(
-      `https://panda-market-api.vercel.app/products?${query}`
+      `${PRODUCTS_URL}/${productId}/comments?limit=5`
     );
-    const result = await response.json();
-    return result;
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const body = await response.json();
+    return body;
   } catch (error) {
-    console.error(error);
+    console.error("fetch error:", error);
+    throw error;
   }
 }
