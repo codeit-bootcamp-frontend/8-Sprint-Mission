e3d0c76904b124a0b55ade945a6fc155f1cd553c
김영주 <breadjoo1014@naver.com>
2024-07-05 15:29:05 +0900

    rename(mediaQuery.css): 미디어쿼리 디자인 각 페이지 CSS파일로 통합

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
 import "./assets/styles/global.css";
-import "./assets/styles/mediaquery.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
