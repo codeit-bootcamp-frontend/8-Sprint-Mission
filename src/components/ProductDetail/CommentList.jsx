a4d823977af28b913ac4e08bced83d25253a29d2
Jeongmin Oh <ojm51@naver.com>
2024-07-05 23:16:24 +0900

    feat(CommentList): 코멘트가 없는 경우를 처리하는 컴포넌트 추가

--- src/components/ProductDetail/CommentList.jsx
+++ src/components/ProductDetail/CommentList.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 import "./CommentList.css";
+import inquiryEmptyImage from "../../assets/images/Img_inquiry_empty.png";
+
 import Comment from "./Comment";
 
 function CommentList({ comments }) {
@@ -11,38 +13,54 @@ function CommentList({ comments }) {
   };
 
   const buttonClassName = isInputFill
-    ? "add-ask-button active"
-    : "add-ask-button";
+    ? "add-inquiry-button active"
+    : "add-inquiry-button";
 
   const placeholder =
     "개인정보를 공유 및 요청하거나 명예 훼손, 무단 광고, 불법 정보 유포 시 모니터링 후 삭제될 수 있으며, " +
     "이에 대한 민형사상 책임은 게시자에게 있습니다.";
 
   return (
-    <div className="ask-wrapper">
-      <label className="ask-label" htmlFor="ask">
-        문의하기
-      </label>
-      <textarea
-        className="ask-content"
-        id="ask"
-        placeholder={placeholder}
-        onChange={handleValueChange}
-      ></textarea>
-      <button className={buttonClassName} disabled={!isInputFill}>
-        등록
-      </button>
+    <>
+      <div className="add-inquiry-wrapper">
+        <label className="inquiry-label" htmlFor="inquiry">
+          문의하기
+        </label>
+        <textarea
+          className="inquiry-content"
+          id="inquiry"
+          placeholder={placeholder}
+          onChange={handleValueChange}
+        ></textarea>
+        <button className={buttonClassName} disabled={!isInputFill}>
+          등록
+        </button>
+      </div>
 
-      <ul className="comment-lists">
-        {comments.map((comment) => {
-          return (
-            <li key={comment.id}>
-              <Comment comment={comment} />
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+      <div className="inquiry-wrapper">
+        {comments.length !== 0 ? (
+          <ul className="comment-lists">
+            {comments.map((comment) => {
+              return (
+                <li key={comment.id}>
+                  <Comment comment={comment} />
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <div className="no-inquiry-wrapper">
+            <img
+              src={inquiryEmptyImage}
+              alt="아직 문의가 없습니다."
+              width="200px"
+              height="200px"
+            />
+            <div className="no-inquiry-text">아직 문의가 없습니다.</div>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
