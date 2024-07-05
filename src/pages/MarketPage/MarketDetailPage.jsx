fded6458e50fd4f064cd03ca5f57df8471af433d
ayoooyh <127219927+ayoooyh@users.noreply.github.com>
2024-07-06 00:48:57 +0900

    에러안나게 수정

--- src/pages/MarketPage/MarketDetailPage.jsx
+++ src/pages/MarketPage/MarketDetailPage.jsx
@@ -17,40 +17,41 @@ function MarketDetailPage() {
   }, [parm.id]);
 
   return (
-    // Object.keys(data).length >= 1 && (
-    <div className="MarketDetailItem">
-      <img
-        className="MarketDetailItem-img"
-        src={data.images}
-        alt={data.title}
-      />
-      <div className="MarketDetailContentContainer">
-        <div className="MarketMarketDetailContent">
-          <p className="itemName">{data.name}</p>
-          <p className="itemPrice">{data.price}원</p>
-          <hr className="line" />
-          <p className="itemIntroduce">상품소개 </p>
-          <p className="itemDescription">{data.description}</p>
-          <p className="itemTagName">상품 태그</p>
-          <div style={{ display: "flex", gap: "10px" }}>
-            {data.tags &&
-              data.tags.map((v, i) => {
-                return (
-                  <span className="itemTag" key={i}>
-                    # {v}
-                  </span>
-                );
-              })}
+    Object.keys(data).length >= 1 && (
+      <div className="MarketDetailItem">
+        <img
+          className="MarketDetailItem-img"
+          src={data.images}
+          alt={data.title}
+        />
+        <div className="MarketDetailContentContainer">
+          <div className="MarketMarketDetailContent">
+            <p className="itemName">{data.name}</p>
+            <p className="itemPrice">{data.price.toLocaleString()}원</p>
+            <hr className="line" />
+            <p className="itemIntroduce">상품소개 </p>
+            <p className="itemDescription">{data.description}</p>
+            <p className="itemTagName">상품 태그</p>
+            <div style={{ display: "flex", gap: "10px" }}>
+              {data.tags &&
+                data.tags.map((v, i) => {
+                  return (
+                    <span className="itemTag" key={i}>
+                      # {v}
+                    </span>
+                  );
+                })}
+            </div>
+          </div>
+
+          <div className="likeContainer">
+            <span className="like">
+              <Heart /> {data.favoriteCount}
+            </span>
           </div>
         </div>
-
-        <div className="likeContainer">
-          <span className="like">
-            <Heart /> {data.favoriteCount}
-          </span>
-        </div>
       </div>
-    </div>
+    )
   );
 }
 
