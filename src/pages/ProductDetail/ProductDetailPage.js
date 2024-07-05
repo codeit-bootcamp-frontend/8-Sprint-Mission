d88e430ccdb6b45f93e2489dab23f8255a5b82dd
“choi-youngsun” <sunn7931@naver.com>
2024-07-05 18:14:35 +0900

    상품 상세 페이지 아이템 정렬 css 일부 완성

--- src/pages/ProductDetail/ProductDetailPage.js
+++ src/pages/ProductDetail/ProductDetailPage.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import likeIcon from "../../images/heart_inactive.png";
 import backIcon from "../../images/icon_back.png";
 import CommentList from "./CommentList";
 import { getProductById } from "../../api";
+import "./ProductDetail.css";
 
 function ProductdetailPage() {
   const { productId } = useParams();
@@ -32,24 +33,40 @@ function ProductdetailPage() {
 
   return (
     <div>
-      <p>상품 사진</p>
-      <h1>{product.name}</h1>
-      <p>{product.price}</p>
-      <h2>상품 소개</h2>
-      <p>{product.description}</p>
-      <h2>상품 태그</h2>
-      <div>{product.tags}</div>
-      <div>
-        <img src={likeIcon} alt="회색 좋아요 아이콘"></img>
-        <p>{product.favoriteCount}</p>
-      </div>
-      <lable for="ask-input">문의하기</lable>
+      <section className="product-section">
+        <img
+          className="product-img"
+          src={product.images[0]}
+          alt="상품 이미지"
+        />
+        <div className="detail-container">
+          <h1 className="product-name">{product.name}</h1>
+          <p className="product-price">{product.price}</p>
+          <h2>상품 소개</h2>
+          <p className="product-description">{product.description}</p>
+          <h2>상품 태그</h2>
+          <div className="product-tags">{product.tags}</div>
+          <div className="like-container">
+            <img
+              className="like-icon"
+              src={likeIcon}
+              alt="회색 좋아요 아이콘"
+            ></img>
+            <p className="product-favoriteCount">{product.favoriteCount}</p>
+          </div>
+        </div>
+      </section>
+      <lable className="ask-label" htmlFor="ask-input">
+        문의하기
+      </lable>
       <input id="ask-input"></input>
       <CommentList />
-      <button>
-        목록으로 돌아가기
-        <img src={backIcon} alt="돌아가기 아이콘" />
-      </button>
+      <Link to="/market">
+        <button className="button-back">
+          목록으로 돌아가기
+          <img src={backIcon} alt="돌아가기 아이콘" />
+        </button>
+      </Link>
     </div>
   );
 }
