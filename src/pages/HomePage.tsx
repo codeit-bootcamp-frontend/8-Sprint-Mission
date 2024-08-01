5dc197f44280bc499b34f5c78bed9b6ed4228c75
leehj322 <gudwns8956@naver.com>
2024-08-02 03:19:36 +0900

    feat(HomePage): Footer 컴포넌트 생성

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import GlobalNavBar from "components/@shared/GlobalNavBar";
+import Footer from "components/home/Footer";
 
 function HomePage() {
   return (
     <>
       <GlobalNavBar isMain={true} isLogin={false} />
+      <Footer />
     </>
   );
 }
