215c18f91e324890917e70c9c033439ef4c1b153
김영주 <breadjoo1014@naver.com>
2024-08-02 02:02:36 +0900

    chore: 테일윈드 css 설치

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: { pretendard: ["Pretendard", "sans-serif"] },
+      colors: {
+        brand: "#3692ff",
+        banner: "#cfe5ff",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  ],
+};
