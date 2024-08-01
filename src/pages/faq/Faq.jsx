a5499ca8af715b190ec176ebaec127204a9fba90
wonsic3686@gmail.com <wonsic3686@gmail.com>
2024-08-01 17:40:38 +0900

    feat: 리액트 타입스크립트 프로젝트 생성

--- src/pages/faq/Faq.jsx
+++
@@ -1,9 +0,0 @@
-function Faq() {
-  return (
-    <>
-      <h1>임시 FAQ 페이지</h1>
-    </>
-  );
-}
-
-export default Faq;
--- src/pages/privacy/Privacy.jsx
+++
@@ -1,9 +0,0 @@
-function Privacy() {
-  return (
-    <>
-      <h1>임시 이용약관 페이지</h1>
-    </>
-  );
-}
-
-export default Privacy;
