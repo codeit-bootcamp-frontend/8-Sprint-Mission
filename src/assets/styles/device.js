21e857406b3ef7147aa50b459e1c3a99b5c9eddd
leehj322 <gudwns8956@naver.com>
2024-08-01 11:28:48 +0900

    env: 스프린트 미션 타입 스크립트 초기 세팅 구성

--- src/assets/styles/device.js
+++
@@ -1,9 +0,0 @@
-const size = {
-  mobile: "767px",
-  tablet: "1199px",
-};
-
-export const device = {
-  mobile: `(max-width: ${size.mobile})`,
-  tablet: `(max-width: ${size.tablet})`,
-};
--- src/pages/Privacy/PrivacyPage.js
+++
@@ -1,5 +0,0 @@
-function PrivacyPage() {
-  return <h1>privacy page</h1>;
-}
-
-export default PrivacyPage;
