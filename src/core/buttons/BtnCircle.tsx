5188a86bba46875f8ae253e72ec9c10fac248d37
L1m3Kun <sungku2757@gmail.com>
2024-08-02 21:05:29 +0900

    fix Circle Button css Styled-component 적용

--- src/core/buttons/BtnCircle.tsx
+++ src/core/buttons/BtnCircle.tsx
@@ -1,10 +1,27 @@
-import "./buttons.css";
+import { styled } from "styled-components";
+
+const CircleBtn = styled.button<{isActive:boolean}>`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  border: none;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 19.09px;
+  background-color: {({isActive}) => isActive ? #3f80ed : white};
+  border: 1px solid #e5e7eb;
+  color: {({isActive}) => isActive ? white : #6b7280};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+`;
 
 interface BtnCircleProps {
   onClick?: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
   disabled?: boolean;
   isActive?: boolean;
-  children?: string | React.ReactNode;
+  children?: React.ReactNode;
 }
 
 const BtnCircle = ({
@@ -13,14 +30,11 @@ const BtnCircle = ({
   isActive = false,
   children = "",
 }: BtnCircleProps) => {
-  const className = `btn-circle ${isActive ? "active" : ""}`;
-  const handleClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    onClick(e);
-  };
+
   return (
-    <button disabled={disabled} className={className} onClick={handleClick}>
+    <CircleBtn disabled={disabled} isActive={isActive} onClick={onClick}>
       {children}
-    </button>
+    </CircleBtn>
   );
 };
 
