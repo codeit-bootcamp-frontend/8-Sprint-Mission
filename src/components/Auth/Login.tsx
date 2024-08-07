c63118f59b2e8004f00fb77ca85faeaff42577f9
KimJeongHyeon <kjh985264@gmail.com>
2024-08-07 19:37:20 +0900

    Feat : LoginMenu 컴포넌트 import, placeholder 추가

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,23 +1,22 @@
-import { useState } from 'react';
-import AuthHeader from './AuthHeader';
-import Section from '../../ui/Section/Section';
-import Input from '../../ui/FormComponents/Input';
-import Button from '../../ui/Button/Button';
-import styles from './Register.module.css';
-import { InitialValue, ChangeValueType } from './@types/Auth';
+import { useState } from "react";
+import AuthHeader from "./AuthHeader";
+import LoginMenu from "./LoginMenu";
+import Section from "../../ui/Section/Section";
+import Input from "../../ui/FormComponents/Input";
+import LinkButton from "../../ui/Button/LinkButton";
+import styles from "./Auth.module.css";
+import { LoginInitialValue, ChangeValueType } from "./@types/Auth";
 
-const INITIAL_VALUE = {
-  email: '',
-  nickName: '',
-  password: '',
-  passwordCheck: '',
+const INITIAL_VALUE: LoginInitialValue = {
+  email: "",
+  password: "",
 };
 
 export default function Register() {
-  const [formValue, setFormValue] = useState<InitialValue>(INITIAL_VALUE);
+  const [formValue, setFormValue] = useState(INITIAL_VALUE);
 
   const handleChangeFormValue: ChangeValueType = (name, value) => {
-    setFormValue(prevData => ({
+    setFormValue((prevData) => ({
       ...prevData,
       [name]: value,
     }));
@@ -33,6 +32,7 @@ export default function Register() {
             type="email"
             name="email"
             label="이메일"
+            placeholder="이메일을 입력해주세요"
             className={styles.inputBox}
             changeValue={handleChangeFormValue}
           />
@@ -41,11 +41,22 @@ export default function Register() {
             type="password"
             name="password"
             label="비밀번호"
+            placeholder="비밀번호를 입력해주세요"
             className={styles.inputBox}
             changeValue={handleChangeFormValue}
           />
-          <Button type="submit" isActive={false} btnName="로그인" />
+          <LinkButton
+            type="submit"
+            className={styles.loginBtn}
+            isActive={false}
+            btnName="로그인"
+          />
         </form>
+        <LoginMenu
+          loginMsg="판다마켓이 처음이신가요?"
+          btnMsg="회원가입"
+          linkPath="/signup"
+        />
       </div>
     </Section>
   );
--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -1,24 +1,24 @@
-import { useState, useEffect } from 'react';
-import AuthHeader from './AuthHeader';
-import Section from '../../ui/Section/Section';
-import Input from '../../ui/FormComponents/Input';
-import Button from '../../ui/Button/Button';
-import styles from './Register.module.css';
-import { RegisterInitialValue, ChangeValueType } from './@types/Auth';
+import { useState } from "react";
+import LoginMenu from "./LoginMenu";
+import AuthHeader from "./AuthHeader";
+import Section from "../../ui/Section/Section";
+import Input from "../../ui/FormComponents/Input";
+import LinkButton from "../../ui/Button/LinkButton";
+import styles from "./Auth.module.css";
+import { RegisterInitialValue, ChangeValueType } from "./@types/Auth";
 
-const INITIAL_VALUE = {
-  email: '',
-  nickName: '',
-  password: '',
-  passwordCheck: '',
+const INITIAL_VALUE: RegisterInitialValue = {
+  email: "",
+  nickName: "",
+  password: "",
+  passwordCheck: "",
 };
 
 export default function Register() {
-  const [formValue, setFormValue] =
-    useState<RegisterInitialValue>(INITIAL_VALUE);
+  const [formValue, setFormValue] = useState(INITIAL_VALUE);
 
   const handleChangeFormValue: ChangeValueType = (name, value) => {
-    setFormValue(prevData => ({
+    setFormValue((prevData) => ({
       ...prevData,
       [name]: value,
     }));
@@ -34,6 +34,7 @@ export default function Register() {
             type="email"
             name="email"
             label="이메일"
+            placeholder="이메일을 입력해주세요"
             className={styles.inputBox}
             changeValue={handleChangeFormValue}
           />
@@ -42,6 +43,7 @@ export default function Register() {
             type="text"
             name="nickName"
             label="닉네임"
+            placeholder="닉네임을 입력해주세요"
             className={styles.inputBox}
             changeValue={handleChangeFormValue}
           />
@@ -50,6 +52,7 @@ export default function Register() {
             type="password"
             name="password"
             label="비밀번호"
+            placeholder="비밀번호를 입력해주세요"
             className={styles.inputBox}
             changeValue={handleChangeFormValue}
           />
@@ -58,11 +61,22 @@ export default function Register() {
             type="password"
             name="passwordCheck"
             label="비밀번호 확인"
+            placeholder="비밀번호를 다시 한 번 입력해주세요"
             className={styles.inputBox}
             changeValue={handleChangeFormValue}
           />
-          <Button type="submit" isActive={false} btnName="회원가입" />
+          <LinkButton
+            type="submit"
+            className={styles.loginBtn}
+            isActive={false}
+            btnName="회원가입"
+          />
         </form>
+        <LoginMenu
+          loginMsg="이미 회원이신가요?"
+          btnMsg="로그인"
+          linkPath="/signin"
+        />
       </div>
     </Section>
   );
