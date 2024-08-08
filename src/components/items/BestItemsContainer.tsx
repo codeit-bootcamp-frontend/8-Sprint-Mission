443b45e60c08541019e5712204f9979639f6733c
김영주 <breadjoo1014@naver.com>
2024-08-08 13:43:00 +0900

    refactor: 제네릭 활용, useFetch

--- src/components/items/BestItemsContainer.tsx
+++ src/components/items/BestItemsContainer.tsx
@@ -3,17 +3,15 @@ import ItemContainer from "./ItemContainer";
 import { getProducts } from "../../core/api";
 import useFetch from "../../lib/hooks/useFetch";
 import countPageItems from "../../lib/utils/countPageItems";
-import { Product } from "../../DTO/product";
+import { ProductResponse } from "../../DTO/product";
 
 function BestItemsContainer() {
   const [pageSize, setPageSize] = useState<number>(countPageItems(1, 2, 4));
-  const { data: itemsData } = useFetch(
+  const { data: itemsData } = useFetch<ProductResponse>(
     getProducts,
     { page: 1, pageSize, orderBy: "favorite" },
     { list: [] }
   );
-
-  const items: Product[] = itemsData.list;
 
   const measurePageSize = () => {
     const newPageSize = countPageItems(1, 2, 4);
@@ -33,7 +31,7 @@ function BestItemsContainer() {
     <section className="flex flex-col gap-6">
       <h1 className="font-bold text-xl">베스트 상품</h1>
       <div className="grid grid-cols-4 gap-4 max-md:grid-cols-1 max-xl:grid-cols-2">
-        {items.map((item) => (
+        {itemsData.list.map((item) => (
           <ItemContainer key={item.id} item={item} />
         ))}
       </div>
