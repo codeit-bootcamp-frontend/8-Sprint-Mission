1f625e1201b45478483a2db8cfe5706043b5dfdc
leehj322 <gudwns8956@naver.com>
2024-08-09 21:39:20 +0900

    feat(BoardsPage): BestPosts 컴포넌트 api 요청 로직 추가

---
+++ src/components/boards/BestPosts.tsx
@@ -0,0 +1,38 @@
+import getArticles, { Article } from "@/apis/getArticles";
+import usePageSize from "@/hooks/usePageSize";
+import { useState, useEffect, useCallback } from "react";
+
+interface BestPostCardProps {
+  article: Article;
+}
+
+function BestPostCard({ article }: BestPostCardProps) {
+  return <article className="w-full h-full bg-slate-400">{article.title}</article>;
+}
+
+export default function BestPosts() {
+  const [bestArticles, setBestArticles] = useState<Article[]>([]);
+  const pageSize = usePageSize(1, 2, 3);
+
+  const handleBestArticleLoad = useCallback(async () => {
+    const { list } = await getArticles({ pageSize, orderBy: "like" });
+    setBestArticles(list);
+  }, [pageSize]);
+
+  useEffect(() => {
+    handleBestArticleLoad();
+  }, [handleBestArticleLoad]);
+
+  return (
+    <section className="w-full">
+      <h2 className="text-gray-900 font-bold text-[18px] md:text-[20px] mb-[16px] md:mb-[24px]">
+        베스트 게시글
+      </h2>
+      <div className="flex h-[198px] md:gap-[16px] xl:h-[169px] xl:gap-[24px]">
+        {bestArticles.map((article) => (
+          <BestPostCard key={article.id} article={article} />
+        ))}
+      </div>
+    </section>
+  );
+}
