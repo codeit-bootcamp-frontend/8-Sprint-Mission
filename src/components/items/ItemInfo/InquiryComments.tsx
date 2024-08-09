8973be9e34b334bf81df8dda6d0dda8f6eba86b5
김영주 <breadjoo1014@naver.com>
2024-08-10 03:29:07 +0900

    feat: 문의 내용이 없을 경우 처리

--- src/components/items/ItemInfo/InquiryComments.tsx
+++ src/components/items/ItemInfo/InquiryComments.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import { getProductComments } from "../../../core/api";
 import { INITIAL_COMMENTS } from "../../../constants";
 import useFetch from "../../../lib/hooks/useFetch";
 import { countTime } from "../../../lib/utils/countTime";
 import { CommentResponse } from "DTO/comment";
-import backIcon from "../../../assets/images/ic_back.png";
+import ReturnButton from "components/common/UI/ReturnButton";
+import emptyInquiryImage from "assets/images/img_inquiry_empty.png";
 
 interface InquiryCommentsProps {
   productId: number | undefined;
@@ -19,35 +19,48 @@ function InquiryComments({ productId, limit }: InquiryCommentsProps) {
     INITIAL_COMMENTS
   );
 
-  const navigate = useNavigate();
+  const comments: CommentResponse[] = Array.isArray(commentsData)
+    ? commentsData
+    : [];
 
   return (
     <section className="flex flex-col content-center gap-[40px] ">
       <ul className="w-full flex flex-col content-start flex-grow gap-6">
-        {commentsData.map((comment: CommentResponse) => (
-          <li
-            className="w-full flex flex-col text-gray-800 gap-[30px]"
-            key={comment.id}
-          >
-            <p>{comment.content}</p>
-            <div className="grid gap-1 content-center grid-rows-2 grid-cols-2">
-              <img className="grid w-10 h-10 mr-2" src={comment.writer.image} />
-              <span className="grid text-sm text-gray-600">
-                {comment.writer.nickname}
-              </span>
-              <p className="grid text-xs text-gray-400">{countTime(comment)}</p>
-            </div>
-            <div className="border-2 border-gray-100" />
+        {comments.length === 0 ? (
+          <li className="flex flex-col items-center gap-2">
+            <img
+              src={emptyInquiryImage}
+              alt="빈 문의 내용"
+              className="w-[196px] h-[196px]"
+            />
+            <p className="text-gray-400">아직 문의가 없어요.</p>
           </li>
-        ))}
+        ) : (
+          comments.map((comment: CommentResponse) => (
+            <li
+              className="w-full flex flex-col text-gray-800 gap-[30px]"
+              key={comment.id}
+            >
+              <p>{comment.content}</p>
+              <div className="grid gap-1 content-center grid-rows-2 grid-cols-2">
+                <img
+                  className="grid w-10 h-10 mr-2"
+                  src={comment.writer.image}
+                  alt="작성자"
+                />
+                <span className="grid text-sm text-gray-600">
+                  {comment.writer.nickname}
+                </span>
+                <p className="grid text-xs text-gray-400">
+                  {countTime(comment)}
+                </p>
+              </div>
+              <div className="border-2 border-gray-100" />
+            </li>
+          ))
+        )}
       </ul>
-      <button
-        className="w-[248px] bg-brand rounded-[40px] mx-auto flex content-center justify-center p-3 text-gray-100 text-lg gap-3"
-        onClick={() => navigate(-1)}
-      >
-        목록으로 돌아가기
-        <img src={backIcon} className="h-6" />
-      </button>
+      <ReturnButton buttonText="목록으로 돌아가기" />
     </section>
   );
 }
