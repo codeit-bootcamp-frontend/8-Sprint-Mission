5c161ade38dff29fae0905fa893af76590ae4fa4
L1m3Kun <sungku2757@gmail.com>
2024-08-09 15:24:15 +0900

    fix: styled components type fixed

--- panda-market/styled.d.ts
+++ panda-market/styled.d.ts
@@ -3,9 +3,51 @@ import 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
-      primary: string;
-      secondary: string;
-      error: string;
+      primary: {
+        50: string;
+        100: string;
+        200: string;
+        400: string;
+        500: string;
+        600: string;
+        700: string;
+        800: string;
+        900: string;
+      };
+      secondary: {
+        50: string;
+        100: string;
+        200: string;
+        400: string;
+        500: string;
+        600: string;
+        700: string;
+        800: string;
+        900: string;
+      };
+      error: {
+        50: string;
+        100: string;
+        200: string;
+        400: string;
+        500: string;
+        600: string;
+        700: string;
+        800: string;
+        900: string;
+      };
+      coolGray: {
+        50: string;
+        100: string;
+        200: string;
+        400: string;
+        500: string;
+        600: string;
+        700: string;
+        800: string;
+        900: string;
+      };
+      white: string;
     };
   }
 }
