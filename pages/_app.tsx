ef6784f28e270bde74bad11185c26ee017355df5
ayoooyh <127219927+ayoooyh@users.noreply.github.com>
2024-08-09 23:25:39 +0900

    assets 추가 및 header 부분 완료

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "@/components/Header";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <Head /> */}
+      <Component {...pageProps} />
+    </>
+  );
 }
