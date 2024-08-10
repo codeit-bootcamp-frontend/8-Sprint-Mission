547a576a05f8394b0f9e912527ccc7809c77d8c8
wonsic3686@gmail.com <wonsic3686@gmail.com>
2024-08-10 20:37:10 +0900

    feature: Header 추가

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -3,44 +3,50 @@ import styles from './Header.module.scss';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import clsx from 'clsx';
-import imgPandaMarketLogo from '@/src/assets/images/logo/logo.svg';
 import Image from 'next/image';
+import imgPandaMarketLogo from '@/src/assets/images/logo/logo.svg';
+import imgProfile from '@/src/assets/images/icons/ic_profile.svg';
 
 const Header = ({}) => {
   const { pathname } = useRouter();
 
   return (
-    <header className={styles.navbar}>
-      <div className={styles.navbar__left}>
-        <a href="/">
+    <header className={styles['navbar']}>
+      <div className={styles['navbar__left']}>
+        <Link href="/">
           <Image
-            className={styles.navbar__homeLogo}
+            className={styles['navbar__homeLogo']}
             src={imgPandaMarketLogo}
             alt="판다마켓 홈"
           />
-        </a>
-        <nav className={styles.navbar__menu}>
-          <ol>
-            <li
-              className={clsx(styles.navbar__menuItem, {
-                [styles['navbar__menuItem--active']]: pathname === '/boards',
-              })}
-            >
-              <Link href={'/boards'}>자유게시판</Link>
-            </li>
-            <li
-              className={clsx(styles.navbar__menuItem, {
-                [styles['navbar__menuItem--active']]: pathname === '/items',
-              })}
-            >
-              <Link href={'/items'}>중고마켓</Link>
-            </li>
-          </ol>
+        </Link>
+        <nav className={styles['navbar__menu']}>
+          <Link
+            href={'/boards'}
+            className={clsx(styles['navbar__menuItem'], {
+              [styles['navbar__menuItem--active']]: pathname === '/boards',
+            })}
+          >
+            자유게시판
+          </Link>
+
+          <Link
+            href={'/items'}
+            className={clsx(styles['navbar__menuItem'], {
+              [styles['navbar__menuItem--active']]: pathname === '/items',
+            })}
+          >
+            중고마켓
+          </Link>
         </nav>
       </div>
-      <a href="/signin" id="login-link-button" className="button">
-        로그인
-      </a>
+      <Link href="/signin" id="login-link-button" className="button">
+        <Image
+          className={styles['navbar__profile']}
+          src={imgProfile}
+          alt="프로필"
+        />
+      </Link>
     </header>
   );
 };
