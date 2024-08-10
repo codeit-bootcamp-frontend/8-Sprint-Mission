837acf53c2b13297ea704a86d5348d5d24c6b235
kjh9852 <kjh985264@gmail.com>
2024-08-11 05:50:00 +0900

    Feat : 반응형 게시글 개수 로직 추가

--- components/Post/BestPost.tsx
+++ components/Post/BestPost.tsx
@@ -5,14 +5,47 @@ import BestPostList from './BestPostList';
 import styles from './BestPost.module.css';
 import { PostListProps } from './@types/Post';
 
+const DEVICE_SIZE = {
+  tablet: 1028,
+  mobile: 768,
+};
+
+function getBestPostSize() {
+  if (window.innerWidth <= DEVICE_SIZE.mobile) {
+    return 1;
+  } else if (window.innerWidth <= DEVICE_SIZE.tablet) {
+    return 2;
+  } else return 3;
+}
+
 export default function BestPost() {
   const [bestposts, setBestPosts] = useState<PostListProps[]>([]);
+  const [pageSize, setPageSize] = useState<number | null>(3);
 
   async function getBestPost() {
-    const res = await axios.get('/articles?pageSize=3&orderBy=like');
+    const res = await axios.get(`/articles?pageSize=${pageSize}&orderBy=like`);
     const BestPosts = res.data.list;
     setBestPosts(BestPosts);
   }
+
+  useEffect(() => {
+    if (pageSize !== null) {
+      getBestPost();
+    }
+  }, [pageSize]);
+
+  useEffect(() => {
+    const updatePageSize = () => {
+      const newPageSize = getBestPostSize();
+      setPageSize(newPageSize);
+    };
+    updatePageSize();
+
+    window.addEventListener('resize', updatePageSize);
+    return () => {
+      window.removeEventListener('resize', updatePageSize);
+    };
+  }, []);
 
   useEffect(() => {
     getBestPost();
