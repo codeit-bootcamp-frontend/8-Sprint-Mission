fb48c67e7a8734856b1b9672974a9b50cc7ff112
yamdang <youm4000@naver.com>
2024-08-10 17:31:35 +0900

    refactor: .next파일 push내용 삭제

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/board":["static\u002Fchunks\u002Fpages\u002Fboard.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fboard"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
