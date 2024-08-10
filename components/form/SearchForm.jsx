f7cc89900e60ccb9a281cc1f9233e41f68d75d2a
nayoonju <bubblenana@bubblecon.io>
2024-08-11 06:00:08 +0900

    fix: 검색/드롧다운 기능 팻치 수정

--- components/form/SearchForm.jsx
+++ components/form/SearchForm.jsx
@@ -1,22 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import styles from "./SearchForm.module.scss";
 
-function SearchForm({ initialValue = "" }) {
-  const [value, setValue] = useState(initialValue);
+function SearchForm({ onSearch }) {
+  const [keyword, setKeyword] = useState("");
   const router = useRouter();
 
   function handleChange(e) {
-    setValue(e.target.value);
+    setKeyword(e.target.value);
   }
+
+  useEffect(() => {
+    const currentKeyword = router.query.q || "";
+    setKeyword(currentKeyword);
+  }, [router.query.q]);
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (!value) {
-      return router.push("/");
+    if (!keyword) {
+      return router.push("/boards");
     }
-    return router.push(`/search?q=${value}`);
+    onSearch(keyword);
+    router.push(`/boards?q=${keyword}`);
   }
+
   return (
     <form onSubmit={handleSubmit} className={styles["search-form"]}>
       <input
@@ -24,7 +31,7 @@ function SearchForm({ initialValue = "" }) {
         className={styles.search}
         placeholder="검색할 상품을 입력해주세요"
         name="q"
-        value={value}
+        value={keyword}
         onChange={handleChange}
       />
     </form>
