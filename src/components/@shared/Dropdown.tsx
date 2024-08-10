0a2dfc25cdc3a6e5d9a0bd29144a5b9b68a78311
leehj322 <gudwns8956@naver.com>
2024-08-10 13:54:11 +0900

    fix(BoardsPage): hydration error 해결
    - useCurrentDevice hook을 이용해서 조건부 렌더링 했을때 해당 조건이 현재 window 상태와 일치하지 않으면 랜더링을 다르게 해서 생기는 에러여서 반응형으로 처리함

--- src/components/@shared/Dropdown.tsx
+++ src/components/@shared/Dropdown.tsx
@@ -21,7 +21,6 @@ export default function Dropdown<T extends string>({
   onItemChange,
 }: DropdownProps<T>) {
   const [isOpen, toggleIsOpen] = useToggle();
-  const currentDevice = useCurrentDevice();
 
   const handleItemClick = (value: T) => {
     onItemChange(value);
@@ -30,22 +29,14 @@ export default function Dropdown<T extends string>({
 
   return (
     <div className="relative">
-      <button onClick={toggleIsOpen} suppressHydrationWarning={true}>
-        {currentDevice === "mobile" ? (
-          <div className="flex justify-center items-center w-[42px] h-[42px] bg-gray border-gray-200 border-[1px] rounded-[8px]">
-            <Image width={24} height={24} src="/images/ic_dropdown.png" alt="드롭다운 버튼" />
-          </div>
-        ) : (
-          <div className="flex justify-between items-center w-[130px] h-[42px] px-[20px] py-[9px] text-gray-800 bg-gray text-[16px] font-normal border-[1px] rounded-[12px]">
-            {itemDict[currentItemValue]}
-            <Image
-              width={24}
-              height={24}
-              src="/images/ic_dropdown_arrow.png"
-              alt="드롭다운 화살표"
-            />
-          </div>
-        )}
+      <button onClick={toggleIsOpen}>
+        <div className="flex md:hidden justify-center items-center w-[42px] h-[42px] bg-gray border-gray-200 border-[1px] rounded-[8px]">
+          <Image width={24} height={24} src="/images/ic_dropdown.png" alt="드롭다운 버튼" />
+        </div>
+        <div className="hidden md:flex justify-between items-center w-[130px] h-[42px] px-[20px] py-[9px] text-gray-800 bg-gray text-[16px] font-normal border-[1px] rounded-[12px]">
+          {itemDict[currentItemValue]}
+          <Image width={24} height={24} src="/images/ic_dropdown_arrow.png" alt="드롭다운 화살표" />
+        </div>
       </button>
       <ul
         className={`${
