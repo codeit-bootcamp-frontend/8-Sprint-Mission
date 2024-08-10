c296e4f8763a336577413f67a930daca06748b39
leehj322 <gudwns8956@naver.com>
2024-08-10 15:47:22 +0900

    refactor(BoardsPage): 게시판 페이지 디자인 수정

--- src/pages/boards/index.tsx
+++ src/pages/boards/index.tsx
@@ -6,7 +6,7 @@ export default function Boards() {
   return (
     <>
       <GlobalNavBar isMain={false} isLogin />
-      <main className="mt-[16px] md:mt-[24px] mx-[16px] md:mx-[24px] xl:mx-auto xl:w-[1200px]">
+      <main className="flex flex-col gap-[24px] mt-[16px] md:mt-[24px] mx-[16px] md:mx-[24px] xl:mx-auto xl:w-[1200px]">
         <BestPosts />
         <PostList />
       </main>
