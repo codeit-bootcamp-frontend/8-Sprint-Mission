1a1e286ff46f626826c9ca1c2549566d4397966c
김영주 <breadjoo1014@naver.com>
2024-08-12 02:32:50 +0900

    feat: Main 컴포넌트, 홈페이지 확인 속성 추가

--- src/components/common/Layout/Main.tsx
+++ src/components/common/Layout/Main.tsx
@@ -1,13 +1,19 @@
 interface MainProps {
   children: React.ReactNode;
+  isHome?: boolean;
   className?: string;
 }
 
-function Main({ children, className }: MainProps) {
+function Main({ children, isHome, className }: MainProps) {
   return (
     <main
-      className={`${className} max-w-[1200px] pt-24 pb-12 mx-auto gap-6 flex flex-col font-pretendard 
-      max-md:w-full max-md:px-4 max-xl:w-full max-xl:px-6`}
+      className={`
+        ${
+          isHome
+            ? "w-full pt-[70px] gap-6 flex flex-col font-pretendard "
+            : `${className} max-w-[1200px] pt-24 pb-12 mx-auto gap-6 flex flex-col font-pretendard max-md:w-full max-md:px-4 max-xl:w-full max-xl:px-6`
+        }
+      `}
     >
       {children}
     </main>
