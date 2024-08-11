913aa82144d1ed1517481c4f75572993f7499d7d
김영주 <breadjoo1014@naver.com>
2024-08-12 02:27:23 +0900

    refactor: PageLayout 추가, header 여부에 따라 페이지 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,34 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/common/Layout/Header";
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import Items from "./pages/Items";
-import ItemInfo from "./pages/ItemInfo";
-import AddItem from "./pages/AddItem";
-import Boards from "./pages/Boards";
-import NotFound from "./pages/NotFound";
+import PageLayout from "components/common/Layout/PageLayout";
+import Home from "pages/Home";
+import SignIn from "pages/SignIn";
+import Items from "pages/Items";
+import ItemInfo from "pages/ItemInfo";
+import AddItem from "pages/AddItem";
+import Boards from "pages/Boards";
+import NotFound from "pages/NotFound";
+import SignUp from "pages/SignUp";
 
-const Router: React.FC = () => {
+const App = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* Header 포함 페이지 */}
+        <Route element={<PageLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/items" element={<Items />} />
+          <Route path="/items/:productId" element={<ItemInfo />} />
+          <Route path="/addItem" element={<AddItem />} />
+          <Route path="/boards" element={<Boards />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+        {/* Header 불포함 페이지 */}
         <Route path="/signIn" element={<SignIn />} />
-        <Route path="/items" element={<Items />} />
-        <Route path="/items/:productId" element={<ItemInfo />} />
-        <Route path="/addItem" element={<AddItem />} />
-        <Route path="/boards" element={<Boards />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/signUp" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
-export default Router;
+export default App;
