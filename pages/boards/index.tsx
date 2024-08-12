fdb8835a7699a06ffd9471a3baa180b63e1c2527
JeongHyeonKim <kjh985264@gmail.com>
2024-08-12 17:46:55 +0900

    Feat : prefetch 로직 추가

--- pages/boards/index.tsx
+++ pages/boards/index.tsx
@@ -1,11 +1,32 @@
-import BestPost from '@/components/Post/BestPost';
-import Post from '@/components/Post/Post';
+import BestPost from "@/components/Post/BestPost";
+import Post from "@/components/Post/Post";
+import { PostListProps } from "@/components/Post/@types/Post";
+import { getPostList } from "@/utils/api";
 
-export default function Board() {
+interface AllPropsListProps {
+  initialPosts: PostListProps[];
+}
+
+export async function getServerSideProps() {
+  const query = {
+    orderBy: "recent",
+    keyword: "",
+    pageSize: 10,
+  };
+  const res = await getPostList({ query });
+  const posts = res;
+  return {
+    props: {
+      initialPosts: posts,
+    },
+  };
+}
+
+export default function Board({ initialPosts }: AllPropsListProps) {
   return (
     <>
       <BestPost />
-      <Post />
+      <Post initialPosts={initialPosts} />
     </>
   );
 }
