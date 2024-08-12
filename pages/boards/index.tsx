925ea24e8e1eabcb244169da99acec3c97796f04
JeongHyeonKim <kjh985264@gmail.com>
2024-08-12 18:58:16 +0900

    Feat : 메타 태그 수정

--- pages/boards/index.tsx
+++ pages/boards/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import BestPost from "@/components/Post/BestPost";
 import Post from "@/components/Post/Post";
 import { PostListProps } from "@/components/Post/@types/Post";
@@ -25,6 +26,12 @@ export async function getServerSideProps() {
 export default function Board({ initialPosts }: AllPropsListProps) {
   return (
     <>
+      <Head>
+        <title>판다마켓 - 자유게시판</title>
+        <meta name="description" content="일상의 모든 물건을 거래해보세요" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <BestPost />
       <Post initialPosts={initialPosts} />
     </>
