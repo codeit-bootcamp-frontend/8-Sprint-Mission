6f23edd1672d71d40b30300d67dd972aa941bd54
nayoonju <bubblenana@bubblecon.io>
2024-08-12 11:11:41 +0900

    fix: ts 오류 수정

--- lib/utils/formatDate.ts
+++ lib/utils/formatDate.ts
@@ -1,5 +1,4 @@
-// utils/formatDate.js
-export function formatDate(dateString) {
+export function formatDate(dateString: string) {
   const date = new Date(dateString);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 1을 더함
