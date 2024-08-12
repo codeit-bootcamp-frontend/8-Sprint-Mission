295d8f426bf3783b5dfff02b4774784538a50e43
kjh9852 <kjh985264@gmail.com>
2024-08-13 05:22:12 +0900

    Feat : 로딩 컴포넌트 추가

---
+++ components/Loading/EllipsisLoading.tsx
@@ -0,0 +1,20 @@
+import styles from './EllipsisLoading.module.css';
+
+interface LoadingProps {
+  className?: string;
+}
+
+export default function Loading({ className }: LoadingProps) {
+  return (
+    <div className={`${styles.loaderContainer} ${className}`}>
+      <div className={styles.loaderWrapper}>
+        <div className={styles.ldsEllipsis}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
