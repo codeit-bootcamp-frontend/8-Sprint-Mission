ba94edca7be36f57e85ea42354f78adb94662c47
Jeongmin Oh <ojm51@naver.com>
2024-08-13 21:56:06 +0900

    chore: tailwind css 설치

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{html,js,jsx,ts,tsx}",
+    "./pages/**/*.{html,js,jsx,ts,tsx}",
+    "./styles/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
