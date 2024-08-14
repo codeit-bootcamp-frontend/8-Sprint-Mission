f6ef68e8f0bd9453dfeef6a132d26d7deab30c56
leehj322 <gudwns8956@naver.com>
2024-08-14 21:44:53 +0900

    feat(BoardsPage): Link 컴포넌트 추가

--- src/components/boards/PostListHeader.tsx
+++ src/components/boards/PostListHeader.tsx
@@ -3,6 +3,8 @@ import { orderOption } from "@/apis/getArticles";
 import Dropdown from "../@shared/Dropdown";
 import SearchBar, { SearchBarProps } from "../@shared/SearchBar";
 import BlueButton from "../@shared/BlueButton";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 interface PostListHeader extends SearchBarProps {
   currentOrderOption: orderOption;
@@ -19,12 +21,15 @@ export default function PostListHeader({
   onOrderChange,
   onKeywordChange,
 }: PostListHeader) {
+  const { pathname } = useRouter();
   return (
     <div className="flex flex-col gap-[16px] md:gap-[24px]">
       <div className="flex justify-between items-center">
         <h2 className="text-gray-900 font-bold text-[18px] md:text-[20px]">게시글</h2>
         <div className="w-[88px] h-[42px]">
-          <BlueButton shape="default">글쓰기</BlueButton>
+          <Link href={`${pathname}/add`}>
+            <BlueButton shape="default">글쓰기</BlueButton>
+          </Link>
         </div>
       </div>
       <div className="flex items-center gap-[12px] xl:gap-[16px]">
