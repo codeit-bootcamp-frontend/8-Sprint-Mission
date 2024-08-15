31d357094003d3e731f5219cd991cd7a8463fd4b
L1m3Kun <sungku2757@gmail.com>
2024-08-16 00:19:31 +0900

    fix: addboard 헤드 form과 합성

--- panda-market/src/c_widget/addBoard/header/ui/AddBoardHeader.style.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrapper = styled.section`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 64.625rem;
-  align-self: stretch;
-`;
