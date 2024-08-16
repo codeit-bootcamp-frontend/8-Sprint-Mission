e56be3fb492f6fe76b42b1411ce72029a296dc35
JeongHyeonKim <kjh985264@gmail.com>
2024-08-16 13:36:25 +0900

    Feat : page query 타입 추가

--- hooks/usePostList.tsx
+++ hooks/usePostList.tsx
@@ -1,30 +1,26 @@
-import { useState } from 'react';
-
-interface queryType {
-  query: {
-    orberBy?: string | string[] | undefined;
-    pageSize?: string | number;
-    keyword?: string;
-  };
-}
+import { useState } from "react";
+import { QueryType } from "./types/usePostListTypes";
 
 export default function usePostList<T>(
-  getFn: (query: queryType) => Promise<T[]>,
+  getFn: (query: QueryType) => Promise<{ list: T[]; totalCount: number }>,
   initialList: T[]
 ) {
   const [loading, setLoading] = useState<Boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const [dataList, setDataList] = useState<T[]>(initialList);
-  const fetchPost = async (query: queryType) => {
+  const [totalItem, setTotalItem] = useState<number | null>();
+
+  const fetchPost = async (query: QueryType) => {
     setLoading(true);
     try {
       const res = await getFn(query);
-      setDataList(res);
+      setDataList(res.list);
+      setTotalItem(res.totalCount);
     } catch (error) {
       if (error instanceof Error) {
         setError(error.message);
       } else {
-        setError('예기치 않은 오류가 발생했습니다.');
+        setError("예기치 않은 오류가 발생했습니다.");
       }
     } finally {
       setLoading(false);
@@ -34,6 +30,7 @@ export default function usePostList<T>(
   return {
     loading,
     error,
+    totalItem,
     dataList,
     fetchPost,
   };
