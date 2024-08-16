27b2f1f44a4b7c8b7ad848d6a6e3f97a7ab0541f
L1m3Kun <sungku2757@gmail.com>
2024-08-16 23:59:02 +0900

    feat: 메타 태그 및 페이지 타이틀, 파비콘 설정

--- panda-market/pages/_document.tsx
+++ panda-market/pages/_document.tsx
@@ -28,7 +28,35 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="ko">
-        <Head></Head>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>판다마켓</title>
+          <meta property="og:title" content="판다마켓" />
+          <meta
+            property="og:description"
+            content="일상의 모든 물건을 거래해보세요"
+          />
+          <meta
+            property="og:image"
+            content="source/images/headline/Img_home_top.png"
+          />
+          <meta property="og:url" content="https://storepanda.netlify.app" />
+
+          <meta property="twitter:title" content="판다마켓" />
+          <meta
+            property="twitter:description"
+            content="일상의 모든 물건을 거래해보세요"
+          />
+          <meta
+            property="twitter:image"
+            content="source/images/headline/Img_home_top.png"
+          />
+          <meta
+            property="twitter:url"
+            content="https://storepanda.netlify.app"
+          />
+          <meta property="twitter:card" content="summary" />
+        </Head>
         <body>
           <Main />
           <NextScript />
