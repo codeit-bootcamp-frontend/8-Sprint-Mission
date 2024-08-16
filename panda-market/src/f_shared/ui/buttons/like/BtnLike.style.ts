d0382fe05c464e278332cc5b13b71230ae4eba5e
L1m3Kun <sungku2757@gmail.com>
2024-08-16 20:20:13 +0900

    fix: 좋아요 버튼 사이즈 및 상태 추가

--- panda-market/src/f_shared/ui/buttons/like/BtnLike.style.ts
+++ panda-market/src/f_shared/ui/buttons/like/BtnLike.style.ts
@@ -1,9 +1,43 @@
+import { colors } from '@/f_shared/config';
 import styled from 'styled-components';
 
-export const Button = styled.button``;
+export type LikeBtnSize = 'small' | 'large';
 
-export const ButtonContent = styled.div``;
+interface ButtonProps {
+  $size: LikeBtnSize;
+}
 
-export const ButtonContentIcon = styled.div``;
+export const Button = styled.button<ButtonProps>`
+  display: flex;
+  height: ${({ $size }) => ($size === 'large' ? '2.5rem' : '2rem')};
+  padding: 0.25rem 0.75rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.625rem;
+  flex-shrink: 0;
 
-export const ButtonContentText = styled.p``;
+  border-radius: 35px;
+  border: 1px solid ${colors.secondary[200]};
+  background-color: ${colors.white};
+`;
+
+export const ButtonContent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+`;
+
+export const ButtonContentIcon = styled.div<ButtonProps>`
+  width: ${({ $size }) => ($size === 'large' ? '2rem' : '1.5rem')};
+  height: ${({ $size }) => ($size === 'large' ? '2rem' : '1.5rem')};
+`;
+
+export const ButtonContentText = styled.p`
+  color: ${colors.coolGray[500]};
+
+  /* pretendard/lg-16px-medium */
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 1.625rem; /* 162.5% */
+`;
--- panda-market/src/f_shared/ui/buttons/like/BtnLike.tsx
+++ panda-market/src/f_shared/ui/buttons/like/BtnLike.tsx
@@ -1,23 +1,28 @@
+import { useState } from 'react';
 import { IconLike } from '../../icon';
 
 import * as S from './BtnLike.style';
 
 interface BtnLikeProps {
+  btnSize?: S.LikeBtnSize;
   likeCount: number;
   onClick: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
   isLike?: boolean;
 }
 
 export const BtnLike = ({
+  btnSize = 'large',
   likeCount,
   onClick,
   isLike = false,
 }: BtnLikeProps) => {
+  const [isActive, setIsActive] = useState<boolean>(isLike);
+
   return (
-    <S.Button onClick={onClick}>
+    <S.Button onClick={onClick} $size={btnSize}>
       <S.ButtonContent>
-        <S.ButtonContentIcon>
-          <IconLike isLike={isLike} />
+        <S.ButtonContentIcon $size={btnSize}>
+          <IconLike isLike={isActive} />
         </S.ButtonContentIcon>
         <S.ButtonContentText>{likeCount}</S.ButtonContentText>
       </S.ButtonContent>
