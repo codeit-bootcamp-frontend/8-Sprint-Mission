ecb5b0431da7361790e33a6351c16e8a7791993d
JeongHyeonKim <kjh985264@gmail.com>
2024-08-16 13:34:48 +0900

    Refactor : NavList 분리

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,7 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import NAVIGATION_LIST from '@/utils/NAVIGATION_LIST';
-import styles from './Header.module.css';
-
-const navList = NAVIGATION_LIST.map(list => (
-  <li key={list.name}>
-    <Link href={list.path}>{list.name}</Link>
-  </li>
-));
+import Image from "next/image";
+import Link from "next/link";
+import NavList from "./NavList";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
@@ -16,22 +10,34 @@ export default function Header() {
         <div className={styles.headerNav}>
           <div className={styles.logoBox}>
             <Link href="/" className={styles.logo}>
-              <Image fill src="/images/logo@2.png" alt="판다마켓 로고" />
+              <Image
+                fill
+                src="/images/logo@2.png"
+                alt="판다마켓 로고"
+                sizes="width:153px, height: 51px"
+                priority
+              />
             </Link>
             <Link href="/" className={styles.mobileLogo}>
               <Image
                 fill
                 src="/images/mobile_logo@2.png"
                 alt="판다마켓 모바일 로고"
+                sizes="width:81px, height: 40px"
+                priority
               />
             </Link>
           </div>
-          <div className={styles.navList}>
-            <ul>{navList}</ul>
-          </div>
+          <NavList />
         </div>
         <Link href="/" className={styles.profile}>
-          <Image fill src="/images/profile@2.png" alt="사용자 프로필" />
+          <Image
+            fill
+            src="/images/profile@2.png"
+            alt="사용자 프로필"
+            sizes="width:40px, height: 40px"
+            priority
+          />
         </Link>
       </div>
     </header>
