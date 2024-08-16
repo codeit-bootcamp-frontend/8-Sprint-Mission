d9bacdd8db66f7b01105a76339764eb1cf00c18a
L1m3Kun <sungku2757@gmail.com>
2024-08-16 20:21:05 +0900

    fix: 상세 페이지 데이터 타입 추가

--- panda-market/src/entities/articles/getBoardDetail.ts
+++ panda-market/src/entities/articles/getBoardDetail.ts
@@ -1,11 +1,11 @@
 import { API_PATH } from '@/f_shared/config';
 import { instance } from '../instance';
-import { BoardDetailParams } from './types';
+import { Article, BoardDetailParams } from './types';
 
 export const getBoardDetail = async ({ boardItemId }: BoardDetailParams) => {
   return await instance
-    .get(API_PATH.articlePath.detail(boardItemId))
-    .then((res) => res)
+    .get<Article>(API_PATH.articlePath.detail(boardItemId))
+    .then((res) => res.data)
     .catch((e) => {
       throw new Error(e);
     });
