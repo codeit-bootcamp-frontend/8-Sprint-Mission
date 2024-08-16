1225efbd00886053ea542a7c01488125a826e017
L1m3Kun <sungku2757@gmail.com>
2024-08-16 17:40:02 +0900

    feat: 이미지 에러 추가

---
+++ panda-market/src/f_shared/ui/image/Img.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import { useState } from 'react';
+import { match } from 'ts-pattern';
+
+import DefaultLargeImg from '@/f_shared/assets/images/default_item/default_large.png';
+
+type ImageErrorType = { type: 'success' } | { type: 'default' };
+
+interface ImgProps {
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+}
+
+export const Img = ({ src, alt, width, height }: ImgProps) => {
+  const [isError, setIsError] = useState<boolean>(false);
+  const imageErrorType: ImageErrorType = {
+    type: !isError ? 'success' : 'default',
+  };
+
+  const ImgContent = match(imageErrorType)
+    .with({ type: 'success' }, () => (
+      <Image
+        src={src}
+        alt={alt}
+        onError={() => setIsError(true)}
+        fill
+        width={width}
+        height={height}
+      />
+    ))
+    .with({ type: 'default' }, () => (
+      <Image
+        src={DefaultLargeImg}
+        alt={alt}
+        fill
+        width={width}
+        height={height}
+      />
+    ))
+    .exhaustive();
+  return <>{ImgContent}</>;
+};
