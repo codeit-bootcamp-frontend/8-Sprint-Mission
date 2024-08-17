22431003b5508e031452f6e6e3f80bf9f47083bc
kjh9852 <kjh985264@gmail.com>
2024-08-18 06:32:53 +0900

    Feat : 포스트 댓글, 포스트 불러오는 로직 추가

--- utils/api.ts
+++ utils/api.ts
@@ -1,21 +1,43 @@
-import axios from "@/lib/axios";
+import axios from '@/lib/axios';
 
-interface queryProps {
+interface QueryType {
   query: {
-    orderBy?: "recent" | "like";
+    orderBy?: 'recent' | 'like';
     keyword?: string;
     pageSize?: string | number;
     page?: number;
   };
 }
 
-export async function getPostList({ query }: queryProps) {
+interface CommentQueryType {
+  query: {
+    limit: number;
+  };
+}
+
+export async function getPostList({ query }: QueryType) {
   const { orderBy, keyword, pageSize, page } = query;
   const res = await axios.get(
-    `/articles?orderBy=${orderBy}&keyword=${keyword ?? ""}&pageSize=${
+    `/articles?orderBy=${orderBy}&keyword=${keyword ?? ''}&pageSize=${
       pageSize ?? 6
     }&page=${page ?? 1}`
   );
   const postList = res.data;
   return postList;
+}
+
+export async function getPost(id: string | string[] | undefined) {
+  const res = await axios.get(`/articles/${id}`);
+  const post = res.data;
+  return post;
+}
+
+export async function getPostComment(
+  { query }: CommentQueryType,
+  id: string | string[] | undefined
+) {
+  const { limit } = query;
+  const res = await axios.get(`/articles/${id}/comments?limit=${limit ?? 10}`);
+  const post = res.data;
+  return post;
 }
