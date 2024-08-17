1340577ba8c47c79c402fc7f1893c5ba509debbb
L1m3Kun <sungku2757@gmail.com>
2024-08-17 20:08:10 +0900

    feat: board 컨텍스트 설정 추가

--- panda-market/src/f_shared/config/context/board.ts
+++ panda-market/src/f_shared/config/context/board.ts
@@ -1,4 +1,30 @@
-import { Article } from '@/entities';
 import { createContext } from 'react';
+import { Article, ArticleCommentsResponse } from '@/entities';
 
-export const BoardDetailContext = createContext<Article | null>(null);
+export interface BoardDetailContextType {
+  boardData: Article;
+  boardComments: ArticleCommentsResponse;
+}
+
+const INITIAL_BOARD: Article = {
+  id: 0,
+  title: '',
+  content: '',
+  image: '',
+  createdAt: '',
+  likeCount: 0,
+  updatedAt: '',
+  writer: {
+    id: 0,
+    nickname: '',
+  },
+};
+const INITIAL_BOARD_COMMENTS: ArticleCommentsResponse = {
+  list: [],
+  nextCursor: 0,
+};
+
+export const BoardDetailContext = createContext<BoardDetailContextType>({
+  boardData: INITIAL_BOARD,
+  boardComments: INITIAL_BOARD_COMMENTS,
+});
