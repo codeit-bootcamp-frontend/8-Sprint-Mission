95d0b76bdfe62ee7e24170b6389036d48b8fe3c3
seunghyun <tmdgus7048@naver.com>
2024-08-18 17:52:41 +0900

    addBoard페이지 추가

--- pages/addBoard/index.tsx
+++ pages/addBoard/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import styles from "./index.module.css";
+import styles from "./addBoard.module.css";
+import plusIcon from "@/public/images/icons/ic_plus.svg";
+import Image from "next/image";
 
-const addBoard = () => {
+const AddBoard = () => {
   return (
     <form className={styles.addBoardForm}>
       <div className={styles.addBoardHeader}>
@@ -9,36 +11,40 @@ const addBoard = () => {
         <button className={styles.addBoardBtn}>등록</button>
       </div>
       <div className={styles.addBoardInputSection}>
-        <label htmlFor="" className={styles.addBoardLabel}>
+        <label htmlFor="addBoardTitle" className={styles.addBoardLabel}>
           *제목
         </label>
         <input
           type="text"
+          id="addBoardTitle"
           name="title"
           className={styles.addBoardInput}
           placeholder="제목을 입력해주세요"
         />
       </div>
       <div className={styles.addBoardInputSection}>
-        <label htmlFor="" className={styles.addBoardLabel}>
+        <label htmlFor="addBoardContent" className={styles.addBoardLabel}>
           *내용
         </label>
         <textarea
           name="contents"
-          id=""
+          id="addBoardContent"
           className={styles.addBoardTextarea}
           placeholder="내용을 입력해 주세요"
         ></textarea>
       </div>
       <div className={styles.addBoardInputSection}>
-        <label htmlFor="" className={styles.addBoardLabel}></label>
-        <div>
-          <label htmlFor=""></label>
-          <input type="file" />
+        <span className={styles.addBoardLabel}>상품 이미지</span>
+        <div className={styles.fileInputSection}>
+          <label className={styles.fileInputLabel} htmlFor="articleImg">
+            <Image src={plusIcon} alt="plusIcon" />
+            <span>이미지 등록</span>
+          </label>
+          <input id="articleImg" type="file" />
         </div>
       </div>
     </form>
   );
 };
 
-export default addBoard;
+export default AddBoard;
