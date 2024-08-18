5fd390bb0c4f78dab83a2572664e2f59e6586edd
seunghyun <tmdgus7048@naver.com>
2024-08-19 03:24:17 +0900

    등록버튼 활성/비활성화 기능 추가

--- pages/addBoard/index.tsx
+++ pages/addBoard/index.tsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { ChangeEvent, useCallback, useEffect, useState } from "react";
 import styles from "./addBoard.module.css";
 import plusIcon from "@/public/images/icons/ic_plus.svg";
 import Image from "next/image";
+import { AddArticle } from "@/types/types";
 
 const AddBoard = () => {
+  const [values, setValues] = useState<AddArticle>({
+    title: "",
+    content: "",
+    image: null,
+  });
+  const [isDisabledSubmit, setIsDisabledSubmit] = useState(true);
+
+  const isDisabledCheck = useCallback(() => {
+    const { title, content } = values;
+    if (title && content) {
+      setIsDisabledSubmit(false);
+    } else {
+      setIsDisabledSubmit(true);
+    }
+  }, [values]);
+
+  const updateFormValues = (
+    name: keyof AddArticle,
+    value: AddArticle[keyof AddArticle]
+  ) => {
+    setValues((preValues) => ({
+      ...preValues,
+      [name]: value,
+    }));
+  };
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    updateFormValues(name as keyof AddArticle, value);
+  };
+
+  useEffect(() => {
+    isDisabledCheck();
+  }, [isDisabledCheck]);
   return (
     <form className={styles.addBoardForm}>
       <div className={styles.addBoardHeader}>
         <h1> 게시글 쓰기</h1>
-        <button className={styles.addBoardBtn}>등록</button>
+        <button className={styles.addBoardBtn} disabled={isDisabledSubmit}>
+          등록
+        </button>
       </div>
       <div className={styles.addBoardInputSection}>
         <label htmlFor="addBoardTitle" className={styles.addBoardLabel}>
@@ -20,6 +59,8 @@ const AddBoard = () => {
           name="title"
           className={styles.addBoardInput}
           placeholder="제목을 입력해주세요"
+          value={values.title}
+          onChange={handleChange}
         />
       </div>
       <div className={styles.addBoardInputSection}>
@@ -27,11 +68,13 @@ const AddBoard = () => {
           *내용
         </label>
         <textarea
-          name="contents"
+          name="content"
           id="addBoardContent"
           className={styles.addBoardTextarea}
           placeholder="내용을 입력해 주세요"
-        ></textarea>
+          value={values.content}
+          onChange={handleChange}
+        />
       </div>
       <div className={styles.addBoardInputSection}>
         <span className={styles.addBoardLabel}>상품 이미지</span>
