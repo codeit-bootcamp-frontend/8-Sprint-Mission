c82360ae83263dcd86e2da0a6d1a80b9d36e75b3
“choi-youngsun” <sunn7931@naver.com>
2024-08-18 19:55:35 +0900

    style: 불필요한 주석 제거

--- utils/TimeTrimmer.ts
+++ utils/TimeTrimmer.ts
@@ -1,7 +1,7 @@
 function DateTrimmer(date: string) {
   const newDate = new Date(date);
   const year = newDate.getFullYear();
-  const month = String(newDate.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 1을 더합니다
+  const month = String(newDate.getMonth() + 1).padStart(2, "0");
   const day = String(newDate.getDate()).padStart(2, "0");
 
   return `${year}.${month}.${day}`;
