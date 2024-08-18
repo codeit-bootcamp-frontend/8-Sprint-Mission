d3e6b4af8efefdc66410faf29b3e8c6be0136a6e
nayoonju <bubblenana@bubblecon.io>
2024-08-18 19:54:41 +0900

    fix: image upload 컴포넌트 오류 개선

--- components/form/FileInput.tsx
+++ components/form/FileInput.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useRef, useState, ChangeEvent, MouseEvent } from "react";
+import { useState, ChangeEvent } from "react";
 import Image from "next/image";
 import styles from "./FileInput.module.scss";
-import inputImg from "@/public/img/product/sample2.png";
 import Icon from "@/components/ui/Icon";
 
 // Props 타입 정의
@@ -13,45 +12,20 @@ interface FileInputProps {
 }
 
 function FileInput({ label, name, value, onChange }: FileInputProps) {
-  const [preview, setPreview] = useState<string>(
-    value ? URL.createObjectURL(value) : inputImg
-  );
-  const [isBoxVisible, setIsBoxVisible] = useState<boolean>(!!value);
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [imagePreviewUrl, setImagePreviewUrl] = useState("");
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0] || null;
 
     if (file) {
-      onChange(name, file);
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        if (reader.result) {
-          setPreview(reader.result as string);
-          setIsBoxVisible(true);
-        }
-      };
-      reader.readAsDataURL(file);
-    } else {
-      onChange(name, null);
-      setPreview(inputImg);
-      setIsBoxVisible(false);
+      const imageUrl = URL.createObjectURL(file);
+      setImagePreviewUrl(imageUrl);
     }
   };
 
-  const handleImageError = () => {
-    setPreview(inputImg);
+  const handleDelete = () => {
+    setImagePreviewUrl("");
   };
-
-  useEffect(() => {
-    if (value) {
-      setPreview(URL.createObjectURL(value));
-      setIsBoxVisible(true);
-    } else {
-      setPreview(inputImg);
-      setIsBoxVisible(false);
-    }
-  }, [value]);
 
   return (
     <div className="input-group">
@@ -62,20 +36,26 @@ function FileInput({ label, name, value, onChange }: FileInputProps) {
           aria-label="이미지 등록 버튼"
         >
           <input
-            ref={inputRef}
-            name={name}
+            id="image-upload"
             className="sr-only"
             type="file"
-            onChange={handleChange}
-            accept="image/jpeg,image/png"
+            onChange={handleImageChange}
+            accept="image/*"
           />
           <Icon type="plus" size="lg" />
           <span>이미지 등록</span>
         </label>
-        {isBoxVisible && (
+        {imagePreviewUrl && (
           <div className={styles["image-add-box"]}>
-            <Image src={preview} alt="상품 이미지" onError={handleImageError} />
-            <Icon type="remove" size="md" alt="이미지 제거 버튼" />
+            <Image src={imagePreviewUrl} alt="상품 이미지" fill />
+            <button
+              type="button"
+              onClick={handleDelete}
+              className={styles["btn-delete"]}
+              aria-label="이미지 제거 버튼"
+            >
+              <Icon type="remove" size="md" />
+            </button>
           </div>
         )}
       </div>
