7f99589c5a3c1140c10ac624a8e0bfd90831924b
hvrain <hvrain3222@naver.com>
2024-08-19 21:22:31 +0900

    Add: landing page tablet

--- components/landing/LandingItem.tsx
+++ components/landing/LandingItem.tsx
@@ -17,20 +17,26 @@ function LandingItem({
   align = 'start',
 }: PropsWithChildren<Props>) {
   return (
-    <div className={`flex flex-col items-${align}`}>
-      <Image width={344} height={259} src={src} alt="landing" />
-      <h2 className="mt-[24px] text-primary-100 font-lg-16px-bold">{badge}</h2>
-      <h3 className="mt-[8px] text-secondary-700 font-2xl-24px-bold">
-        {title}
-      </h3>
-      <p
-        className={cn(
-          'mt-[16px] flex flex-col text-secondary-700 font-lg-16px-medium',
-          `items-${align}`
-        )}
-      >
-        {children}
-      </p>
+    <div className={`w-full items-${align}`}>
+      <div className={`flex flex-col items-${align}`}>
+        <div className="relative aspect-[4/3] w-full">
+          <Image fill src={src} alt="landing" />
+        </div>
+        <h2 className="mt-[24px] text-primary-100 font-lg-16px-bold tablet:font-2lg-18px-bold">
+          {badge}
+        </h2>
+        <h3 className="mt-2 text-secondary-700 font-2xl-24px-bold tablet:mt-4 tablet:font-3xl-32px-bold">
+          {title}
+        </h3>
+        <p
+          className={cn(
+            'mt-4 flex flex-col text-secondary-700 font-lg-16px-medium tablet:mt-6 tablet:font-2lg-18px-medium',
+            `items-${align}`
+          )}
+        >
+          {children}
+        </p>
+      </div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,25 +3,26 @@ import Layout from '@/layouts/Layout';
 import Link from 'next/link';
 import LandingItem from '@/components/landing/LandingItem';
 import Image from 'next/image';
+import cn from '@/lib/utils';
 
 export default function Home() {
   return (
     <main className="mt-[70px]">
-      <div className='h-[540px] w-full bg-[#CFE5FF] bg-[url("../public/landing/Img_home_top@3x.png")] bg-[length:448px_204px] bg-bottom bg-no-repeat'>
-        <div className="mx-auto flex w-[240px] flex-col items-center justify-center gap-[18px] pt-[48px]">
-          <h1 className="flex flex-col items-center justify-center text-[32px] font-bold leading-[44.8px]">
+      <div className='h-[540px] w-full bg-[#CFE5FF] bg-[url("../public/landing/Img_home_top@3x.png")] bg-[length:448px_204px] bg-bottom bg-no-repeat tablet:h-[771px] tablet:bg-[length:744px_340px]'>
+        <div className="mx-auto flex w-full flex-col items-center justify-center gap-[18px] pt-12 tablet:gap-6 tablet:pt-[84px]">
+          <h1 className="flex flex-col items-center justify-center text-[32px] font-bold leading-[44.8px] tablet:flex-row tablet:text-[40px] tablet:leading-[56px]">
             <span>일상의 모든 물건을</span>
             <span>거래해보세요</span>
           </h1>
           <Link
-            className="rounded-full bg-primary-100 px-[71px] py-[11px] text-white font-lg-18px-semibold"
+            className="rounded-full bg-primary-100 px-[71px] py-[11px] text-white font-lg-18px-semibold tablet:px-[124px] tablet:py-4"
             href="/items"
           >
             구경하러 가기
           </Link>
         </div>
       </div>
-      <div className="mt-[52px] flex flex-col items-center justify-center gap-[40px]">
+      <div className="mx-4 mt-[52px] flex flex-col items-center justify-center gap-[40px] tablet:mx-6 tablet:gap-[52px]">
         <LandingItem
           src="/landing/Img_home_01@3x.png"
           badge="Hot item"
@@ -48,15 +49,23 @@ export default function Home() {
           <span>쉽게 등록하세요</span>
         </LandingItem>
       </div>
-      <div className='mt-[83px] h-[540px] w-full bg-[#CFE5FF] bg-[url("../public/landing/Img_home_bottom@3x.png")] bg-[length:375px_198px] bg-bottom bg-no-repeat'>
-        <div className="mx-auto flex w-[240px] flex-col items-center justify-center gap-[18px] pt-[112px]">
-          <h1 className="flex flex-col items-center justify-center text-[32px] font-bold leading-[44.8px]">
+      <div className='mt-[83px] h-[540px] w-full bg-[#CFE5FF] bg-[url("../public/landing/Img_home_bottom@3x.png")] bg-[length:375px_198px] bg-bottom bg-no-repeat tablet:mt-14 tablet:h-[927px] tablet:bg-[length:744px_397px]'>
+        <div className="mx-auto flex w-full flex-col items-center justify-center gap-[18px] pt-[112px] tablet:pt-[201px]">
+          <h1 className="flex flex-col items-center justify-center text-[32px] font-bold leading-[44.8px] tablet:text-[40px] tablet:leading-[56px]">
             <span>믿을 수 있는</span>
             <span>판다마켓 중고 거래</span>
           </h1>
         </div>
       </div>
-      <footer className="grid auto-rows-min grid-cols-[minmax(100px,_1fr)_minmax(33px,_1fr)_minmax(116px,_1fr)] grid-rows-2 items-center gap-y-[60px] bg-[#111322] p-[32px] text-[#CFCFCF]">
+      <footer
+        className={cn(
+          'grid auto-rows-min grid-cols-[minmax(100px,_1fr)_minmax(33px,_1fr)_minmax(116px,_1fr)] grid-rows-2 items-center gap-y-[60px] bg-[#111322] p-[32px] text-[#CFCFCF]',
+          'tablet:grid-cols-[minmax(96px,_1fr)_minmax(84px,_1fr)_minmax(24px,_1fr)_minmax(116px,_1fr)] tablet:grid-rows-1 tablet:pb-[108px]'
+        )}
+      >
+        <span className="col-span-2 row-start-2 text-[#676767] tablet:col-span-1 tablet:row-start-1">
+          ©codeit - 2023
+        </span>
         <span>Privacy Policy</span>
         <span className="text-center">FAQ</span>
         <div className="flex h-[18px] w-[116px] justify-between">
@@ -85,9 +94,7 @@ export default function Home() {
             alt="instagram link"
           />
         </div>
-        <span className="col-span-2 text-[#676767]">©codeit - 2023</span>
       </footer>
-      <div className="mb-[100px]" />
     </main>
   );
 }
