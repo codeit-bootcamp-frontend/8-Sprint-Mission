411d2d08d85d25e81db16a02e696b14d31deb7d6
seunghyun <tmdgus7048@naver.com>
2024-08-20 00:30:27 +0900

    피드백 반환타입추가, try catch문 추가

--- lib/articleApi.ts
+++ lib/articleApi.ts
@@ -1,4 +1,9 @@
-import { ArticlesResponse, GetArticles } from "@/types/types";
+import {
+  Article,
+  ArticlesResponse,
+  CommentsResponse,
+  GetArticles,
+} from "@/types/types";
 import axios from "@/lib/axios";
 import { AxiosResponse } from "axios";
 
@@ -7,28 +12,55 @@ export const getArticles = async ({
   pageSize,
   orderBy,
   keyword,
-}: GetArticles) => {
-  const orderByQuery = orderBy ? `&orderBy=${orderBy}` : "";
-  const keywordQuery = keyword ? `&keyword=${keyword}` : "";
+}: GetArticles): Promise<ArticlesResponse> => {
+  try {
+    const orderByQuery = orderBy ? `&orderBy=${orderBy}` : "";
+    const keywordQuery = keyword ? `&keyword=${keyword}` : "";
 
-  const res: AxiosResponse = await axios.get<ArticlesResponse>(
-    `/articles?page=${page}&pageSize=${pageSize}${orderByQuery}${keywordQuery}`
-  );
-  if (res.status !== 200) throw new Error("getArticles api 오류");
-  const result = res.data;
-  return result;
+    const res: AxiosResponse<ArticlesResponse> =
+      await axios.get<ArticlesResponse>(
+        `/articles?page=${page}&pageSize=${pageSize}${orderByQuery}${keywordQuery}`
+      );
+
+    if (res.status !== 200) {
+      throw new Error("getArticles API 오류");
+    }
+
+    return res.data;
+  } catch (error) {
+    console.error("Error in getArticles:", error);
+    throw error;
+  }
 };
 
-export const getArticle = async (articleId: number) => {
-  const res = await axios.get(`/articles/${articleId}`);
-  if (res.status !== 200) throw new Error("getArticle api 오류");
-  const result = res.data;
-  return result;
+export const getArticle = async (articleId: number): Promise<Article> => {
+  try {
+    const res = await axios.get<Article>(`/articles/${articleId}`);
+    if (res.status !== 200) throw new Error("getArticle api 오류");
+    const result = res.data;
+    return result;
+  } catch (error) {
+    console.error("Error in getArticle:", error);
+    throw error;
+  }
 };
 
-export const getComments = async (articleId: number, limit: number) => {
-  const res = await axios.get(`articles/${articleId}/comments?limit=${limit}`);
-  if (res.status !== 200) throw new Error("getComments api 오류");
-  const result = res.data;
-  return result;
+export const getComments = async (
+  articleId: number,
+  limit: number
+): Promise<CommentsResponse> => {
+  try {
+    const res = await axios.get<CommentsResponse>(
+      `articles/${articleId}/comments?limit=${limit}`
+    );
+
+    if (res.status !== 200) {
+      throw new Error("getComments API 오류");
+    }
+
+    return res.data;
+  } catch (error) {
+    console.error("Error in getComments:", error);
+    throw error;
+  }
 };
