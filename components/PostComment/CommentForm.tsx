eccfc97ec056dd356bb560359d0b4e9140848b20
kjh9852 <kjh985264@gmail.com>
2024-08-20 10:49:39 +0900

    Feat : 댓글 전송 기능 추가

--- components/PostComment/CommentForm.tsx
+++ components/PostComment/CommentForm.tsx
@@ -1,34 +1,55 @@
-import { useState } from "react";
-import styles from "./CommentForm.module.css";
-import TextArea from "../Input/TextArea";
-import LinkButton from "../Button/LinkButton";
+import { useState, SyntheticEvent } from 'react';
+import { useRouter } from 'next/router';
+import styles from './CommentForm.module.css';
+import TextArea from '../Input/TextArea';
+import LinkButton from '../Button/LinkButton';
+import { postNewArticleComment } from '@/utils/api';
 
 export default function CommnetForm() {
-  const [commentValue, setCommentValue] = useState("");
+  const [commentValue, setCommentValue] = useState('');
+  const router = useRouter();
+  const token = localStorage.getItem('token');
+  const { id } = router.query;
 
-  const handleChangeValue = (name = "comment", value) => {
+  const handleChangeValue = (name = 'comment', value: string) => {
     setCommentValue(value);
   };
 
-  const isActive = commentValue.trim() !== "";
+  const isActive = commentValue.trim() !== '';
+
+  const postNewComment = async (postId: string | string[] | undefined) => {
+    const commentData = {
+      content: commentValue,
+    };
+    try {
+      const res = await postNewArticleComment(postId, commentData, token);
+      if (res) {
+        router.reload();
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleSubmitComment = (e: SyntheticEvent) => {
+    e.preventDefault();
+    postNewComment(id);
+  };
 
   return (
-    <>
-      <form className={styles.commentForm}>
-        <TextArea
-          style={{ height: "104px" }}
-          id="comment"
-          name="comment"
-          variant="comment"
-          label="답글달기"
-          value={commentValue}
-          placeholder="댓글을 입력해주세요."
-          changeValue={handleChangeValue}
-        />
-        <div className={styles.commentBtn}>
-          <LinkButton isActive={!isActive} type="submit" btnName="등록" />
-        </div>
-      </form>
-    </>
+    <form onSubmit={handleSubmitComment} className={styles.commentForm}>
+      <TextArea
+        style={{ height: '104px' }}
+        id="comment"
+        name="comment"
+        label="답글달기"
+        value={commentValue}
+        placeholder="댓글을 입력해주세요."
+        changeValue={handleChangeValue}
+      />
+      <div className={styles.commentBtn}>
+        <LinkButton isActive={!isActive} type="submit" btnName="등록" />
+      </div>
+    </form>
   );
 }
