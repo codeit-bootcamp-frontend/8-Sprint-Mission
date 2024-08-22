9778f26690801bee9c9048e617f115b20cbb368f
김영주 <breadjoo1014@naver.com>
2024-08-23 01:58:07 +0900

    refactor: className 변수 범위 변경

--- src/components/@shared/UI/form/FormButton.tsx
+++ src/components/@shared/UI/form/FormButton.tsx
@@ -1,18 +1,23 @@
 interface FormButtonProps {
   isFormValid: boolean;
   className?: string;
+  buttonText?: string;
 }
 
-function FormButton({ isFormValid, className }: FormButtonProps) {
+function FormButton({
+  isFormValid,
+  className,
+  buttonText = "등록",
+}: FormButtonProps) {
   return (
     <button
       type="submit"
-      className={`${className} absolute top-24 right-0 max-xl:right-6 max-md:right-4 text-white w-[74px] h-[42px] rounded-lg ${
+      className={`${className} text-white ${
         isFormValid ? "bg-brand" : "bg-gray-400"
       }`}
       disabled={!isFormValid}
     >
-      등록
+      {buttonText}
     </button>
   );
 }
