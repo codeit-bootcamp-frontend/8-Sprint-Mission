3d4367040242ec582f23aa9bf79a9e766028bc93
wonsic3686@gmail.com <wonsic3686@gmail.com>
2024-08-22 19:33:57 +0900

    chore: tailwind content 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
