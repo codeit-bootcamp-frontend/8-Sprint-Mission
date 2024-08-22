96ed80b9a2ee1f0e34bfdb78ef58f06f180c0328
김영주 <breadjoo1014@naver.com>
2024-08-23 01:46:05 +0900

    rename: assets 폴더로 global.css 이동

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "./index.css";
+import "assets/styles/global.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(<App />);
-
-reportWebVitals();
