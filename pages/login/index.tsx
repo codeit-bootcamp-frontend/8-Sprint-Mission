95005df12a3603206d957a46f55e2ab63cd67797
wonsic3686@gmail.com <wonsic3686@gmail.com>
2024-08-22 19:19:49 +0900

    feat: login signup 페이지 추가

---
+++ pages/login/index.tsx
@@ -0,0 +1,13 @@
+import BasicLayout from '@components/layout/BasicLayout/BasicLayout';
+
+type LoginPageProps = {};
+
+const LoginPage = ({}: LoginPageProps) => {
+  return (
+    <>
+      <BasicLayout></BasicLayout>
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ pages/signup/index.tsx
@@ -0,0 +1,13 @@
+import BasicLayout from '@components/layout/BasicLayout/BasicLayout';
+
+type SignUpPageProps = {};
+
+const SignUpPage = ({}: SignUpPageProps) => {
+  return (
+    <>
+      <BasicLayout></BasicLayout>
+    </>
+  );
+};
+
+export default SignUpPage;
