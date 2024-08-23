9ed002546f5fe624eda66e889fce1757f42caf6e
김영주 <breadjoo1014@naver.com>
2024-08-23 22:46:34 +0900

    rename: 기본 이미지 변수 리네이밍

--- src/core/constants/defaultImages.ts
+++ src/core/constants/defaultImages.ts
@@ -1,8 +1,8 @@
 import defalutItemImg from "assets/images/img_item_default.png";
-import defalutProfileIc from "assets/icons/ic_profile.png";
+import defalutProfileImg from "assets/images/img_profile.png";
 
 export const DEFAULT_IMAGE_URL = "https://example.com/...";
 
 export const DEFAULT_ITEM_IMAGE = defalutItemImg;
 
-export const DEFAULT_PROFILE_ICON = defalutProfileIc;
+export const DEFAULT_PROFILE_IMAGE = defalutProfileImg;
