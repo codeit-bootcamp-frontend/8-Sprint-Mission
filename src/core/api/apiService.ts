f8c9b9056944367ffcfc42d7261d5eb09bfb599b
김영주 <breadjoo1014@naver.com>
2024-08-23 22:45:42 +0900

    chore: Authorization 헤더에 jwt 조건부 전송

--- src/core/api/apiService.ts
+++ src/core/api/apiService.ts
@@ -15,18 +15,30 @@ export async function getFromApi<T>(url: string): Promise<T> {
 }
 
 // POST, 공통 apiCall 함수
-export async function postToApi<T>(url: string, data: T): Promise<T> {
+export async function postToApi<T>(
+  url: string,
+  data: T,
+  jwtToken?: string
+): Promise<T> {
   try {
+    const headers: HeadersInit = {
+      "Content-Type": "application/json",
+    };
+
+    if (jwtToken) {
+      headers["Authorization"] = `Bearer ${jwtToken}`;
+    }
+
     const response = await fetch(url, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: headers,
       body: JSON.stringify(data),
     });
+
     if (!response.ok) {
       throw new Error(`HTTP 에러: ${response.status}`);
     }
+
     return await response.json();
   } catch (error) {
     console.error("API 요청 실패:", error);
