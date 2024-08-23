46c6ddefc9c2eda16089925775a00ba180552c12
leehj322 <gudwns8956@naver.com>
2024-08-23 17:43:32 +0900

    fix(config): 이미지 경로 임시 수정
    - 다른 사람이 비정상적으로 올린 모든 이미지 url에 모두 대응해야하는 문제 임시적으로 해결함
    - 이후에 컴포넌트 상에서 s3 주소만 필터링해서 띄우는 방향으로 수정할 예정

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,22 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
+      // 정상적이지 않은 방법으로 올라온 이미지에 대해 처리하기 위해 임시 사용
+      {
+        protocol: "http",
+        hostname: "**",
+      },
       {
         protocol: "https",
-        hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
-        port: "",
-        pathname: "/Sprint_Mission/user/**",
+        hostname: "**",
       },
+      // 이후에 컴포넌트 상에 띄울때 s3주소만 필터링 해서 띄우고 나머지는 버리는 훅 추가 후 아래 설정으로 사용하기
+      // {
+      //   protocol: "https",
+      //   hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
+      //   port: "",
+      //   pathname: "/Sprint_Mission/user/**",
+      // },
     ],
   },
 };
