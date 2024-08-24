8fc9bdbc18c34e02d38e7e3209702ee9da86d93d
JeongHyeonKim <kjh985264@gmail.com>
2024-08-24 21:38:51 +0900

    Design : 스타일 변경

--- src/router/Root.tsx
+++ src/router/Root.tsx
@@ -1,11 +1,11 @@
-import { Outlet } from 'react-router-dom';
-import Header from '../ui/Header/Header';
+import { Outlet } from "react-router-dom";
+import Header from "../ui/Header/Header";
 
 export default function Root() {
   return (
     <>
       <Header />
-      <main style={{ marginTop: '94px' }}>
+      <main style={{ marginTop: "71px" }}>
         <Outlet />
       </main>
     </>
