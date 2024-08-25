47ba2534df56c0ea51901a281624b07aa345824d
ayoooyh <127219927+ayoooyh@users.noreply.github.com>
2024-08-26 04:55:33 +0900

    addForm 껍데기 완성

--- components/AddBoard/AddForm.tsx
+++ components/AddBoard/AddForm.tsx
@@ -1,4 +1,5 @@
 import styles from "@/components/AddBoard/AddForm.module.css";
+import PlusIcon from "assets/images/icons/ic_plus.svg";
 
 export default function AddForm() {
   return (
@@ -6,6 +7,21 @@ export default function AddForm() {
       <div className={styles.addFormHeader}>
         <span className={styles.articleWrite}>게시글 쓰기</span>
         <button className={styles.enrollment}>등록</button>
+      </div>
+      <div className={styles.writeForm}>
+        <span className={styles.titleFont}>*제목</span>
+        <div className={styles.titleInput}> 제목을 입력해주세요</div>
+        <span className={styles.titleFont}>*내용</span>
+        <div className={styles.contentInput}> 내용을 입력해주세요</div>
+        <span className={styles.titleFont}>이미지</span>
+
+        <div className={styles.addImage}>
+          <div className={styles.plusZone}>
+            <PlusIcon className={styles.plusIcon} />
+            이미지 등록
+          </div>
+          {/* <p className={styles.addImageWord}>이미지 등록</p> */}
+        </div>
       </div>
     </div>
   );
