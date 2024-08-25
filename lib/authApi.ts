7c30c615642314d9e7e056bca98a01932143634c
seunghyun <tmdgus7048@naver.com>
2024-08-26 00:50:56 +0900

    로그인 api추가

--- lib/authApi.ts
+++ lib/authApi.ts
@@ -1,8 +1,8 @@
-import { AddUserRequest } from "@/types/types";
+import { AddUserRequest, LoginUserRequest } from "@/types/types";
 import axios from "@/lib/axios";
 import { API_ENDPOINTS } from "@/lib/address";
 
-export const signupUser = async ({ ...data }: AddUserRequest) => {
+export const signupUser = async (data: AddUserRequest) => {
   try {
     const response = await axios({
       method: "POST",
@@ -12,6 +12,20 @@ export const signupUser = async ({ ...data }: AddUserRequest) => {
     return response;
   } catch (error) {
     console.error("Error in signup: ", error);
+    throw error;
+  }
+};
+
+export const loginUser = async (data: LoginUserRequest) => {
+  try {
+    const response = await axios({
+      method: "POST",
+      url: API_ENDPOINTS.loginUser,
+      data: data,
+    });
+    return response;
+  } catch (error) {
+    console.error("Error in login: ", error);
     throw error;
   }
 };
