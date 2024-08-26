bd4fbc9c2128fa99dc30d147390c53d662726c0a
seunghyun <tmdgus7048@naver.com>
2024-08-26 17:20:09 +0900

    axios interceptors 사용하여 Authorization 헤더에 자동으로 토큰추가

--- lib/axios.ts
+++ lib/axios.ts
@@ -4,4 +4,19 @@ const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default instance;
