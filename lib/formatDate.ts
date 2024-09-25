9cd2ea7f3a942f8a14ecee666ca76df260f17262
Jeongmin Oh <ojm51@naver.com>
2024-09-25 20:22:16 +0900

    chore: 필요 없는 코드 삭제

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,18 +1,3 @@
-// function formatDate(createdAt: string) {
-//   const uploadDate = new Date(createdAt);
-
-//   const formattedDate = uploadDate.toLocaleDateString("ko-KR", {
-//     year: "numeric",
-//     month: "2-digit",
-//     day: "2-digit",
-//   });
-
-//   const finalDate = formattedDate.replace(/\.$/, "");
-
-//   return finalDate;
-// }
-
-// export default formatDate;
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
 
