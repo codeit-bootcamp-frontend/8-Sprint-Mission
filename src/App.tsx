c0f36cdf41d8bf7c5ee60c6ab7400a6ce4dc1fd3
JeongHyeonKim <kjh985264@gmail.com>
2024-10-01 05:41:13 +0900

    feat: Edit페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Root from "./router/Root";
 import ProductsPage from "./router/ProductsPage";
 import AddItemPage from "./router/AddItemPage";
+import EditItemPage from "./router/EditItemPage";
 import ProductDetailPage, {
   loader as productLoader,
 } from "./router/ProductDetailPage";
 import Register from "./components/Auth/Register";
 import Login from "./components/Auth/Login";
 import Home from "./router/Home";
+import { queryClient } from "./utils/http";
+import PrivateRoute from "./router/PrivateRoute";
 
 const router = createBrowserRouter([
   {
@@ -29,6 +34,10 @@ const router = createBrowserRouter([
         loader: productLoader,
       },
       {
+        path: "items/:productId/edit",
+        element: <EditItemPage />,
+      },
+      {
         path: "additem",
         element: <AddItemPage />,
       },
@@ -36,16 +45,29 @@ const router = createBrowserRouter([
   },
   {
     path: "signin",
-    element: <Login />,
+    element: (
+      <PrivateRoute>
+        <Login />
+      </PrivateRoute>
+    ),
   },
   {
     path: "signup",
-    element: <Register />,
+    element: (
+      <PrivateRoute>
+        <Register />
+      </PrivateRoute>
+    ),
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
