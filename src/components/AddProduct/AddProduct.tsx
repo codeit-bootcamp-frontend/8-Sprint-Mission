41dcf55815426a5dc81b36c191c4692bacea337a
JeongHyeonKim <kjh985264@gmail.com>
2024-10-01 05:42:54 +0900

    feat: react query 사용

--- src/components/AddProduct/AddProduct.tsx
+++ src/components/AddProduct/AddProduct.tsx
@@ -1,10 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
 import Section from "../../ui/Section/Section";
 import ProductForm from "./ProductForm";
+import { postProduct, queryClient } from "../../utils/http";
+
+interface Tag {
+  id: string;
+  name?: string;
+}
+
+interface ProductType {
+  imgFile: File | null;
+  tags: Tag[];
+  price: string;
+  description: string;
+  name: string;
+}
 
 export default function AddProduct() {
+  const navigate = useNavigate();
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: (data) => postProduct(data as any, "POST"),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["products"] });
+      navigate("/items");
+    },
+  });
+  const handleAddProductSubmit = (formData) => {
+    mutate(formData);
+  };
   return (
     <Section>
-      <ProductForm />
+      <ProductForm onSubmit={handleAddProductSubmit} />
     </Section>
   );
 }
