dfbcee883bb3b985f6006a6e16d7eeabc6f2891f
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-01 20:42:04 +0900

    fix: SimpleInput forwardRef 적용

--- src/core/ui/inputs/SimpleInput.tsx
+++ src/core/ui/inputs/SimpleInput.tsx
@@ -1,31 +1,40 @@
-interface SimpleInputProps {
-  lable: string;
-  name: string;
-  type: React.InputHTMLAttributes<HTMLInputElement>['type'];
-  placeholder: string;
+import React, { forwardRef } from 'react';
+
+interface SimpleInputProps extends React.ComponentPropsWithoutRef<'input'> {
+  label: string;
   isLabelVisible: boolean;
 }
 
-export default function SimpleInput({
-  lable = '',
-  name = '',
-  type = 'text',
-  placeholder = '',
-  isLabelVisible = true,
-}: SimpleInputProps) {
-  return (
-    <div className="flex w-full justify-center items-start flex-col">
-      {isLabelVisible && (
-        <label className="mb-4 text-base font-semibold text-gray-900">
-          {lable}
-        </label>
-      )}
-      <input
-        className="flex items-start p-6 w-full h-26 rounded-xl bg-gray-50 border-none placeholder-blue"
-        name={name}
-        type={type}
-        placeholder={placeholder}
-      />
-    </div>
-  );
-}
+const SimpleInput = forwardRef<HTMLInputElement, SimpleInputProps>(
+  (
+    {
+      label = '',
+      name = '',
+      type = 'text',
+      placeholder = '',
+      isLabelVisible = true,
+      ...rest
+    },
+    ref
+  ) => {
+    return (
+      <div className="flex w-full justify-center items-start flex-col">
+        {isLabelVisible && (
+          <label className="mb-4 text-base font-semibold text-gray-900">
+            {label}
+          </label>
+        )}
+        <input
+          className="flex items-start p-6 w-full h-26 rounded-xl bg-gray-50 border-none placeholder-blue"
+          name={name}
+          type={type}
+          placeholder={placeholder}
+          ref={ref}
+          {...rest}
+        />
+      </div>
+    );
+  }
+);
+
+export default SimpleInput;
