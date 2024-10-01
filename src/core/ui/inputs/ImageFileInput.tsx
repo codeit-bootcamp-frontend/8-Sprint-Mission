724d15f20bb7789bfcb269a292a53de550e27811
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-01 20:25:44 +0900

    fix: ImageFileInput className cn 적용

--- src/core/ui/inputs/ImageFileInput.tsx
+++ src/core/ui/inputs/ImageFileInput.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import ImgDeafault from '@assets/images/image/img_default.png';
+import { cn } from '@/lib/utils';
 
 interface ImageFileInputProps {
   className?: string;
@@ -44,10 +45,13 @@ function ImageFileInput({
   }, [value, initialPreview]);
 
   return (
-    <div className={`flex justify-start items-center ${className}`}>
+    <div className={cn('flex justify-start items-center', className)}>
       <label
         htmlFor="input-image-file"
-        className="inline-flex justify-center items-center w-[168px] h-[168px] mr-2 rounded-xl bg-gray-100 md:w-[162px] md:h-[162px] md:mr-4 xl:w-[282px] xl:h-[282px] xl:mr-6"
+        className={cn(
+          'inline-flex justify-center items-center w-[168px] h-[168px] mr-2 rounded-xl bg-gray-100 md:w-[162px] md:h-[162px] md:mr-4 xl:w-[282px] xl:h-[282px] xl:mr-6',
+          className
+        )}
       >
         <div className="inline-flex flex-col items-center">
           <div className="text-gray-400 text-base font-normal leading-6">
@@ -65,7 +69,11 @@ function ImageFileInput({
       />
       {value && (
         <>
-          <div className="inline-flex justify-center items-center w-[168px] h-[168px] mr-2 rounded-xl bg-gray-100 md:w-[162px] md:h-[162px] md:mr-4 xl:w-[282px] xl:h-[282px] xl:mr-6">
+          <div
+            className={
+              'inline-flex justify-center items-center w-[168px] h-[168px] mr-2 rounded-xl bg-gray-100 md:w-[162px] md:h-[162px] md:mr-4 xl:w-[282px] xl:h-[282px] xl:mr-6'
+            }
+          >
             <img
               src={(preview || ImgDeafault) as string}
               className="w-full h-full rounded-xl"
@@ -73,7 +81,9 @@ function ImageFileInput({
             />
           </div>
           <button
-            className="relative -top-[60px] -left-[40px] opacity-70 hover:opacity-100 focus:opacity-100 md:-top-[60px] md:-left-[45px] xl:-top-[110px] xl:-left-[60px]"
+            className={
+              'relative -top-[60px] -left-[40px] opacity-70 hover:opacity-100 focus:opacity-100 md:-top-[60px] md:-left-[45px] xl:-top-[110px] xl:-left-[60px]'
+            }
             onClick={handleClearClick}
           />
         </>
@@ -81,4 +91,5 @@ function ImageFileInput({
     </div>
   );
 }
+
 export default ImageFileInput;
